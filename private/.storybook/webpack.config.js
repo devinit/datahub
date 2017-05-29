171ebd877854b8f6ca825b00d22d2572df6556f5
Allan Lukwago <epicallan.al@gmail.com>
2017-05-29 12:12:52 +0300

    storybook config: missing path import correction

--- private/.storybook/webpack.config.js
+++ private/.storybook/webpack.config.js
@@ -1,10 +1,12 @@
+const path = require('path');
 // load the default config generator.
 const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 
 
 const moduleResolver = {
   resolve: {
-    modules: ['node_modules', path.resolve(__dirname, 'private'), path.resolve(__dirname, 'public/semantic')],
+    modules: ['node_modules', path.resolve(__dirname, 'private'),
+              path.resolve(__dirname, 'public/semantic')],
   }
 };
 
