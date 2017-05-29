e66068f66d6d28b6c651b263eb5614b43f10d3f9
Allan Lukwago <epicallan.al@gmail.com>
2017-05-29 16:40:48 +0300

    button example enhancement

--- private/components/atoms/Button/index.js
+++ private/components/atoms/Button/index.js
@@ -9,10 +9,10 @@ type Props = {
   content?: string,
   primary?: boolean, // i got this from the semantic-ui-react code
   secondary?: boolean
-}
+};
 
-const button = ({ onClick, children, content, primary}: Props) => (
-  <Button primary={primary} onClick={onClick}> {children || content} </Button>
-  );
+const button = ({ onClick, children, content, primary, secondary }: Props) => (
+  <Button primary={primary} secondary={secondary} onClick={onClick}> {children || content} </Button>
+);
 
 export default button;
