728403c08c29572660c6fbcd8c5d4ce49a779d6d
Allan Lukwago <epicallan.al@gmail.com>
2017-06-04 09:28:57 +0300

    linting correction

--- private/components/molecules/SocialMedia/index.js
+++ private/components/molecules/SocialMedia/index.js
@@ -6,8 +6,8 @@ type Props = {
   type: string,
   href: string
 };
-const SocialMedia = ({ type, url }: Props) => (
-  <SocialMediaLink href={url}>
+const SocialMedia = ({ type, href }: Props) => (
+  <SocialMediaLink href={href}>
     <i className="{type} square icon" />
   </SocialMediaLink>
 );
