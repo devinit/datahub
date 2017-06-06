422da59aa19f3c33b3602bfcf9385b4ad5edec46
kraiba <krabz01@gmail.com>
2017-06-06 16:51:13 +0300

    move glamourous components to atoms

--- private/components/molecules/YearSlider/index.js
+++ private/components/molecules/YearSlider/index.js
@@ -1,74 +1,12 @@
 import React from 'react';
-import glamorous from 'glamorous';
 import {Container} from 'semantic-ui-react';
-import theme from '../../theme';
-
-const Slider = glamorous.div({
-  display: 'inline-block',
-  position: 'relative',
-  height: '1em',
-  width: '100%',
-  margin: '2.5em .02em',
-  verticalAlign: 'middle',
-  cursor: 'pointer',
-  backgroundColor: theme.lightGray,
-  '& .bubble': {
-    cursor: 'default',
-    top: '-2em',
-    padding: '0.012em 0.12em',
-    fontSize: '.9em',
-    color: theme.greyText,
-  }
-});
-const Input = glamorous.input({
-  whiteSpace: 'nowrap',
-  position: 'absolute',
-  display: 'block',
-  zIndex: 2,
-  margin: '0em',
-  padding: '0em',
-  opacity: '0',
-  height: '100%',
-  left: '0em',
-  width: '100%',
-  cursor: 'pointer',
-});
-
-const Pointer = glamorous.span({
-  position: 'absolute',
-  cursor: 'pointer',
-  width: '2.65em',
-  height: '2.65em',
-  borderRadius: '50%',
-  border: '0.012em solid #666',
-  top: '-0.8em',
-  color: theme.plainWhite,
-  paddingTop: '.6em',
-  textAlign: 'center',
-  backgroundColor: theme.darkRed,
-  zIndex: 1 },
-  (props) => ({
-    left: `${props.left}%`,
-  }));
-
-const Floor = glamorous.span({
-  whiteSpace: 'nowrap',
-  position: 'absolute',
-  display: 'block',
-  zIndex: 1,
-  left: '0em',
-  opacity: 1,
-});
-
-const Ceiling = glamorous.span({
-  whiteSpace: 'nowrap',
-  position: 'absolute',
-  display: 'block',
-  zIndex: 1,
-  right: '0em',
-  opacity: 1,
-});
-
+import {
+  Slider,
+  Floor,
+  Ceiling,
+  Input,
+  Pointer
+} from '../../atoms/YearSlider';
 
 class YearSlider extends React.Component {
   constructor(props) {
