c5cf93a395f74ad5c4452ea2449c2791d25cc660
Allan Lukwago <epicallan.al@gmail.com>
2017-06-06 11:23:59 +0300

    semantic globals to JS and dependencies upgrades

--- private/tools/lessToJs.test.js
+++ private/tools/lessToJs.test.js
@@ -32,7 +32,36 @@ describe('semantic global less to ES6', () => {
     expect(siteAndThemeGroupVars[1].Fonts.length).toBeGreaterThan(2);
   });
   it('should get required whiteListed variable lines', () => {
-    const wantedLines = lessToJs.getWantedVariableLines(lessTheme, lessSite, ['Fonts', 'Brand Colors']);
+    const wantedLines = lessToJs.getWantedVariableLines(lessTheme, lessSite, ['Border Radius', 'Brand Colors']);
+    const wantedAllLines = lessToJs.getWantedVariableLines(lessTheme, lessSite, []);
     expect(wantedLines).toBeDefined();
+    expect(wantedAllLines).toBeDefined();
+  });
+  it('should turn less JS variable objects to css classes i.e styles and evaluate them', async () => {
+    const lessVars = [
+      { name: 'miniSize', value: '(11/14)' },
+      { name: 'tinySize', value: '(12/14)' },
+      { name: 'green', value: '#21BA45' },
+      { name: 'darkgreen', value: 'darken(@green,5)' },
+    ];
+    const lessStyles = lessToJs.lessVarJsObjsToCss(lessVars);
+    const evaluated = await lessToJs.evaluateToCssClasses(lessStyles);
+    expect(lessStyles).toBeDefined();
+    expect(lessStyles.split('\n').length).toBeGreaterThan(3);
+    expect(evaluated).toBeDefined();
+  });
+  it('should turn css into an array of JsObjects', () => {
+    const css = `
+      .tinySize {
+        value: 0.85714286;
+      }
+      .green {
+        value: #21BA45;
+      }
+      .darkgreen {
+        value: #1da43d;
+      }`;
+    const json = lessToJs.evaluatedClassesToJsObjs(css);
+    expect(json[1].name).toBe('green');
   });
 });
