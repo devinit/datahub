702e82305ef305b54fbd484af49b603d8da61ad8
kraiba <krabz01@gmail.com>
2017-06-06 15:05:41 +0300

    add stories

--- private/components/molecules/YearSlider/index.js
+++ private/components/molecules/YearSlider/index.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import glamorous from 'glamorous';
 import {Container} from 'semantic-ui-react';
-import theme from 'components/theme';
-import SearchInput from '../SearchInput';
 
-const SearchTitle = glamorous.div({
-  backgroundColor: theme.black,
-  fontSize: theme.big,
-  textAlign: 'center',
-  color: theme.plainWhite,
-  fontWeight: '700',
-  paddingTop: '1em',
-  paddingBottom: '1em',
-  '& .clickable': {
-    textDecoration: 'underline',
-    cursor: 'pointer',
-  },
-});
 
-class Search extends React.Component {
+class YearSlider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -26,35 +11,43 @@ class Search extends React.Component {
     };
   }
 
-  showInput = () => {
-    if (this.state.showInput) {
-      this.setState({showInput: false});
-    } else {
-      this.setState({showInput: true});
-    }
-  };
 
   render() {
     return (<div>
-      <SearchTitle>
-        <Container>
-          <h2>Explore by
-            <span
-              onClick={this.showInput}
-              className="clickable"
-              role="button"
-            > country
-            </span>
-          </h2>
-        </Container>
-      </SearchTitle>
-      <SearchInput
-        visible={this.state.showInput}
-        countries={['Uganda', 'Kenya', 'Tanzania']}
-        placeholder="Type Your Country Name"
+      <input
+        type="range"
+        className="input low"
+        step="1"
+        min="1990"
+        max="2013"
+        onChange={(e) => console.log(e.target.value)}
       />
     </div>);
   }
 }
 
-export default Search;
+export default YearSlider;
+
+// <div slider="" ng-model="model.year" ng-disabled="isSliderDisabled" floor="1990" ceiling="2013" step-width="1"
+//      precision="0" translate-fn="formatValue(value)"
+//      class="slider ng-untouched ng-valid ng-isolate-scope ng-dirty ng-valid-parse">
+//   <span
+//     class="bar full"
+//     style="white-space: nowrap; position: absolute; display: block; z-index: 1; left: 0px; right: 0px;">
+//   </span>
+//   <span
+//     class="bar steps" style="white-space: nowrap; position: absolute; display: block; z-index: 1;"></span>
+//   <span
+//     class="pointer low"
+//     style="white-space: nowrap; position: absolute; display: block; z-index: 1; left: 315px;"></span>
+//   <span
+//   class="bubble low ng-binding" ng-bind-template="2013"
+//   style="white-space: nowrap; position: absolute; display: block; z-index: 1; left: 315px;">2013</span><span
+//   class="bubble limit floor ng-binding" ng-bind-template="1990"
+//   style="white-space: nowrap; position: absolute; display: block; z-index: 1; left: 0px; opacity: 1;">1990</span><span
+//   class="bubble limit ceiling ng-binding" ng-bind-template="2013"
+//   style="white-space: nowrap; position: absolute; display: block; z-index: 1; left: 317px; opacity: 0;">2013</span>
+//   <input
+//
+//     style="white-space: nowrap; position: absolute; display: block; z-index: 1; margin: 0px; padding: 0px; opacity: 0; height: 100%; left: 0px; width: 100%;"/>
+// </div>
