3662a2af1cc2bcfe81f1bb7901a52c82a59f147c
kraiba <krabz01@gmail.com>
2017-06-08 13:57:25 +0300

    create new shot

--- private/components/molecules/ChartShare/index.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-import ChartShare from '.';
-
-storiesOf('ChartShare', module)
-  .add('Datahub ChartShare', () => <ChartShare />);
