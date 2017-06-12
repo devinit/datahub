bb7f845cb7a2065b3dcc19122542c4f00e500277
kraiba <krabz01@gmail.com>
2017-06-12 17:14:30 +0300

    add centered text element

--- private/components/templates/Generic/index.js
+++ private/components/templates/Generic/index.js
@@ -5,26 +5,13 @@ import type { Element } from 'react';
 import Footer from '../../molecules/Footer';
 import Menu from '../../molecules/Menu';
 
+
 type Props = {
   children?: Element<any>,
   pathName: string
 };
 
 export default ({ children}: Props) => {
-  const mainStyles = {
-    backgroundColor: 'yellow',
-    color: 'cornflowerblue',
-    border: '1px solid lightgreen',
-    borderRight: 'none',
-    borderBottom: 'none',
-    boxShadow: '5px 5px 0 0 lightgreen, 10px 10px 0 0 lightyellow',
-    transition: 'all 0.1s linear',
-    margin: '3rem 0',
-    padding: '1rem 0.5rem',
-  };
-
-  const Main = glamorous.div(mainStyles);
-
   return (
     <div>
       <Menu />
