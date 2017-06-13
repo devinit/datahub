cec228fabd352ecdb89671029842184b81650e41
kraiba <krabz01@gmail.com>
2017-06-13 12:26:55 +0300

    add official flows page

---
+++ private/components/templates/Flows/index.js
@@ -0,0 +1,31 @@
+// @flow
+import React from 'react';
+import glamorous from 'glamorous';
+import {Container, Header} from 'semantic-ui-react';
+import type { Element } from 'react';
+import Generic from '../Generic';
+
+
+export default () => {
+  const headerStyles = {
+    paddingTop: '4em',
+    paddingBottom: '4em',
+  };
+  const HeaderContainer = glamorous.div(headerStyles);
+  return (
+    <Generic pathName="/aid">
+      <Container>
+        <HeaderContainer>
+          <Header as="h1" textAlign="center">
+            <Header.Content>
+              Unbundling other official flows
+              <Header.Subheader>
+                Explore and compare funding priorities for other official flows (OOFs)
+              </Header.Subheader>
+            </Header.Content>
+          </Header>
+        </HeaderContainer>
+      </Container>
+    </Generic>
+  );
+};
