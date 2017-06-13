ef94e1bf7399fdedbe3633086e93b2087ab28b0c
kraiba <krabz01@gmail.com>
2017-06-13 13:27:21 +0300

    fix linting

---
+++ private/components/templates/ContryProfiles/index.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import Profiles from '.';
+
+storiesOf('Templates', module)
+  .add('Country Profiles', () => <Profiles />);
