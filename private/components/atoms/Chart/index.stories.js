73d86efd5e32ca604b4eeae696046c769bc7f161
Ernest Okot <okot08@gmail.com>
2017-06-15 10:30:46 +0300

    added line partitions

--- private/components/atoms/Chart/index.stories.js
+++ private/components/atoms/Chart/index.stories.js
@@ -3,9 +3,11 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import poverty from './poverty.story';
 import resources from './resources.story';
+import govtrfe from './govt-revenue-finance-expenditure.story';
 
 const chartStories = storiesOf('Chart', module)
   .add('Poverty', () => poverty)
-  .add('International Resources', () => resources);
+  .add('International Resources', () => resources)
+  .add('Government revenue, financing and expenditure', () => govtrfe);
 
 export default chartStories;
