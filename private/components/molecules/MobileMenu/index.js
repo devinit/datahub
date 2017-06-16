50fcc33b5f58ee20acfb97c76f8abc9d4b4c591a
kraiba <krabz01@gmail.com>
2017-06-16 07:39:10 +0300

    add menu toggle

--- private/components/molecules/MobileMenu/index.js
+++ private/components/molecules/MobileMenu/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import glamorous from 'glamorous';
 import {Icon} from 'semantic-ui-react';
-import {white, redHeaderColor} from 'components/theme/semantic';
+import {white, redHeaderColor, lightGrey, lightBlack} from 'components/theme/semantic';
 
 export const ToggleButton = glamorous.button({
   position: 'absolute',
@@ -17,7 +17,7 @@ export const ToggleButton = glamorous.button({
   background: redHeaderColor,
   outline: 0,
   '& i': {
-    fontSize: '1.5rem',
+    fontSize: '1.2rem',
     margin: '.28rem 0.25rem 0 0',
 
   },
@@ -25,14 +25,95 @@ export const ToggleButton = glamorous.button({
     display: 'block',
   }
 });
-class MobileMenu extends React.Component {
-  componentWillMount() {
 
+export const Navigation = glamorous.nav({
+  position: 'fixed',
+  top: '4em',
+  right: 0,
+  width: '23em',
+  height: '100%',
+  borderTop: `10px solid ${redHeaderColor}`,
+  paddingBottom: '4em',
+  overflow: 'scroll',
+  background: white,
+  boxShadow: '0 4px 6px rgba(0,0,0,.3)',
+  transform: 'translate(100%,0)',
+  transition: 'all .3s',
+  '& .navigation__item': {
+    position: 'relative',
+    display: 'block',
+    marginBottom: '2px',
+    height: '4em',
+    overflow: 'hidden',
+  },
+  '& .navigation__item-title': {
+    display: 'block',
+    height: '4em',
+    padding: '1.5rem 1.8rem 1.8rem 1.8rem',
+    fontWeight: '700',
+    background: lightGrey,
+    cursor: 'pointer',
+    color: lightBlack,
   }
+}, (props) => ({
+  transform: props.open ? 'translate(0,0)' : 'translate(100%,0)',
+}));
+
+class MobileMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+  toggleNav() {
+    if (this.state.open) {
+      this.setState({open: false});
+    } else {
+      this.setState({open: true});
+    }
+  }
+
   render() {
-    return (<ToggleButton>
-      <Icon name="align justify" />
-    </ToggleButton>);
+    const {open} = this.state;
+    return (<div>
+      <ToggleButton onClick={() => this.toggleNav()}>
+        { open ? <Icon name="cancel" /> : <Icon name="align justify" />}
+      </ToggleButton>
+      <Navigation onBlur={() => this.setState({open: false})} open={open}>
+        <li className="navigation__item">
+          <span className="navigation__item-title small">
+            <a href="">Global Picture</a>
+          </span>
+        </li>
+        <li className="navigation__item">
+          <span className="navigation__item-title small">
+            <a href="">Profiles</a>
+          </span>
+        </li>
+        <li className="navigation__item">
+          <span className="navigation__item-title small">
+            <a href="">Unbundling Aid</a>
+          </span>
+        </li>
+        <li className="navigation__item">
+          <span className="navigation__item-title small">
+            <a href="">Spotlight on Uganda</a>
+          </span>
+        </li>
+        <li className="navigation__item">
+          <span className="navigation__item-title small">
+            <a href="">Other Visualizations</a>
+          </span>
+        </li>
+        <li className="navigation__item">
+          <span className="navigation__item-title small">
+            <a href="">Methodology</a>
+          </span>
+        </li>
+      </Navigation>
+    </div>
+    );
   }
 
 
