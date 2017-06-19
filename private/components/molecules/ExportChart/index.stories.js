7fea3e84db59abc14b37dd3b5eb68c56f43e8036
kraiba <krabz01@gmail.com>
2017-06-19 17:46:23 +0300

    remove export chart

--- private/components/molecules/ExportChart/index.stories.js
+++ private/components/molecules/ExportChart/index.stories.js
@@ -3,5 +3,5 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ExportChart from '.';
 
-storiesOf('Chart share', module)
+storiesOf('Chart share DontTest', module)
   .add('ExportChart', () => <ExportChart />);
