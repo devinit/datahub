2b4ad81c93bb635726cebdce27826e9d6f0d13b7
kraiba <krabz01@gmail.com>
2017-06-19 10:12:25 +0300

    add lead design

--- private/components/atoms/CountryProfiles/LowerTabs/GovernmentFinance.js
+++ private/components/atoms/CountryProfiles/LowerTabs/GovernmentFinance.js
@@ -1,21 +1,35 @@
 import { Container, Header, Grid } from 'semantic-ui-react';
+import glamorous from 'glamorous';
 import React from 'react';
 import {LightBg} from '../Common';
 
+export const Lead = glamorous.span({
+  fontSize: '1.5rem',
+  fontWeight: '300',
+  textAlign: 'center',
+  '& span': {
+    paddingRight: '2px',
+    paddingLeft: '2px',
+    fontWeight: '600',
+  }
+});
 const Government = () => (
   <LightBg>
     <Container>
-      <Grid>
-        <Grid.Column width={16} >
-          <Header
-            textAlign="center"
-            as="h3"
-          >
-            How does Uganda finance and spend its budget?
-            Move the year slider or click a box to drill down.
-          </Header>
-        </Grid.Column>
-
+      <Grid centered>
+        <Grid.Row>
+          <Grid.Column width={8} textAlign="center">
+            <Lead>
+              How does Uganda finance and spend its budget?
+                <span>Move the year slider</span>or <span>click a box</span> to drill down.
+            </Lead>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={8} textAlign="center">
+            <Header as="h2">Government revenue, financing and expenditure</Header>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     </Container>
   </LightBg>
