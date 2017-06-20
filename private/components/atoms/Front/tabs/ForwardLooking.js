e5f485a23804a1ca0cedbb95e932aff3c8ac81b2
kraiba <krabz01@gmail.com>
2017-06-20 07:50:59 +0300

    add foward looking tab

---
+++ private/components/atoms/Front/tabs/ForwardLooking.js
@@ -0,0 +1,28 @@
+import glamorous from 'glamorous';
+import {Container} from 'semantic-ui-react';
+import React from 'react';
+import {TabHeader} from '../Common';
+import Select from '../Select';
+
+const PaneContainer = glamorous.div({
+  paddingBottom: '5em',
+});
+
+const options = [
+  {key: 0, value: 'UK, DFID planned budgets'},
+  {key: 1, value: 'Canada, Global Affairs planned budgets'},
+  {key: 2, value: 'Denmark, DANIDA and Foreign Affairs planned budgets'},
+  {key: 3, value: 'Netherlands, Foreign Affairs planned budgets'},
+  {key: 4, value: 'Belgian Development Cooperation planned budgets'},
+];
+const ForwardLooking = () => (
+  <PaneContainer>
+    <TabHeader>
+      <Container textAlign="center">
+        <Select options={options} />
+      </Container>
+    </TabHeader>
+  </PaneContainer>
+);
+
+export default ForwardLooking;
