8bb67b5bff33dfbf79d9343a93d630e69ea90d6a
kraiba <krabz01@gmail.com>
2017-06-30 13:51:08 +0300

    rename story

--- private/components/templates/Front/index.stories.js
+++ private/components/templates/Front/index.stories.js
@@ -3,5 +3,5 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Front from '.';
 
-storiesOf('Templates Front Page DontTest', module)
+storiesOf('Templates DontTest', module)
   .add('Front Page', () => <Front />);
