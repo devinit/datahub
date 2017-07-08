2285d687eb4a28abf14908f08df0d13f54e4e70c
Allan Lukwago <epicallan.al@gmail.com>
2017-07-08 15:30:56 +0300

    add missing routes and add compression middleware

---
+++ pages/methodology.js
@@ -0,0 +1,23 @@
+// @flow
+/* eslint-disable no-underscore-dangle, max-len, react/no-unescaped-entities */
+import React from 'react';
+import { rehydrate } from 'glamor';
+import withData from 'lib/withData';
+import App from 'components/templates/Methodology';
+import 'lib/offline-install'; // Get our service worker on the page
+
+type Props = {
+  url: {pathname: string}
+}
+// Adds server generated styles to glamor cache.
+// Has to run before any `style()` calls
+// '__NEXT_DATA__.ids' is set in '_document.js'
+if (typeof window !== 'undefined') {
+  rehydrate(window.__NEXT_DATA__.ids);
+}
+
+export default withData((props: Props) => {
+  return (
+    <App pathName={props.url.pathname} />
+  );
+});
