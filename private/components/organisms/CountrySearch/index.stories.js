576606a81013f7e4062d1ae441d3f9bdde6ef86b
Allan Lukwago <epicallan.al@gmail.com>
2017-07-09 22:23:53 +0300

    storyshots and minor changes

--- private/components/organisms/CountrySearch/index.stories.js
+++ private/components/organisms/CountrySearch/index.stories.js
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react';
 import {withApolloProvider} from 'lib/storybook-apollo-addon';
 import CountrySearch from '.';
 
-storiesOf('Country Search', module)
+storiesOf('Country Search DONOT TEST', module)
   .addDecorator(withApolloProvider())
   .add('Country Search With Data', () => <CountrySearch />);
