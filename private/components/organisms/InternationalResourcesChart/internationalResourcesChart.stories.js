28ffc50e4ede93143afba89cebcfff0173f76f7c
Allan Lukwago <epicallan.al@gmail.com>
2017-07-12 16:28:43 +0300

    new storyshots

--- private/components/organisms/InternationalResourcesChart/internationalResourcesChart.stories.js
+++ private/components/organisms/InternationalResourcesChart/internationalResourcesChart.stories.js
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react';
 import {withApolloProvider} from 'lib/storybook-apollo-addon';
 import InternationalResourcesChart from '.';
 
-storiesOf('International Resources chart live', module)
+storiesOf('International Resources chart live DONOT TEST', module)
   .addDecorator(withApolloProvider())
   .add('Area treemap Chart', () => <InternationalResourcesChart id={'uganda'} />);
