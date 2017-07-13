9a75ac14c1f06490db21d64ef36626ecc1b2a13a
Allan Lukwago <epicallan.al@gmail.com>
2017-07-13 10:27:14 +0300

    map component wip

--- private/components/atoms/Map/index.js
+++ private/components/atoms/Map/index.js
@@ -1,60 +1,131 @@
 // @flow
-import React, {Component} from 'react';
-import MapGL from 'react-map-gl';
+import React, {PureComponent} from 'react';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
 import {MapPlaceholder} from '../Container';
 
 type Viewport = {
-  latitude?: number;
-  longitude?: number;
-  zoom?: number;
-  width?: number;
-  height?: number;
+  zoom: number;
+  center: number[];
+  width: number;
+  height: number;
+  attributionControl: boolean;
+  bounds: number[][];
 }
 
 type Props = {
   mapStyle?: string;
-  viewport?: Viewport;
-  token?: string;
+  viewport?: Viewport
 }
 
-class Map extends Component {
+type State = {
+  token: string;
+  mapStyle: string;
+  viewport: Viewport
+}
+type MapBoxOptions = {
+  ...Viewport;
+  style: string;
+  container: HTMLDivElement | string;
+}
 
+type FeatureProperty = {
+  id?: string;
+  ISO?: string;
+  value?: number;
+  type?: string
+}
+
+type PopupItem = {
+  props: FeatureProperty;
+  pos: {lng: number; lat: number};
+}
+
+class Map extends PureComponent {
+  static getToken() {
+    if (!process.env.accessToken) throw new Error('Provide a mapbox access token');
+    const token = process.env.accessToken || 'put here to get rid of flow error';
+    return token;
+  }
+  static tipTemplate(featureProperty: FeatureProperty) {
+    return `<div class = "mapBox-popup"> ${featureProperty.toString()}</div>`;
+  }
   constructor(props?: Props) {
     super(props);
-    this.state = {
-      ...this.state,
-      viewport: {...this.viewport, ...this.props.viewport}
-    };
+    if (props && props.viewport) {
+      const viewport = {...this.state.viewport, ...props.viewport};
+      this.state = {...this.state, viewport};
+    }
+    if (props && props.mapStyle) this.state = {...this.state, mapStyle: props.mapStyle};
+    mapboxgl.accessToken = this.state.token;
+    this._isOnMobile = window.innerWidth < 1200;
   }
-  state = {
-    token: process.env.MapboxAccessToken,
+  state: State = {
+    token: Map.getToken(),
     mapStyle: 'http://178.79.185.236:8080/styles/worldgeojson.json',
     viewport: this.viewport
   };
-
-  viewport = {
-    latitude: 25,
-    longitude: 20,
+  viewport: Viewport = {
     zoom: 1,
+    center: [25, 20],
+    attributionControl: true,
+    minZoom: 0.5,
     bearing: 0,
+    bounds: [
+      [-179, -61], // Southwest coordinates
+      [188, 75]  // Northeast coordinates
+    ],
     scrollZoom: false,
     width: process.browser ? window.innerWidth : 1200,
     height: process.browser && window.innerWidth < 1200 ? 480 : 600
   };
+  _isOnMobile: boolean = false;
+  _map: Object;
+  _nav: Object;
+  _popup: Object & {remove: any};
+  _zoomLevel: number;
 
+  addPopupContent(obj: PopupItem) {
+    this._popup.setLngLat([obj.pos.lng, obj.pos.lat])
+                .setHTML(Map.tipTemplate(obj.props))
+                .addTo(this._map);
+  }
+  mouseHoverEvent() {
+    this._map.on('mousemove', (e) => {
+      // console.log('map bounds', e.lngLat);
+      const features: {properties: any}[] = this._map.queryRenderedFeatures(e.point);
+      // for some reason returns an array of the same event
+      if (!features.length && this._popup) return this._popup.remove();
+      if (!features.length) return false;
+      if (features && !this._popup) this._popup = new mapboxgl.Popup({offset: 10});
+      if (!features[0].properties) return this._popup.remove();
+      return this.addPopupContent({props: features[0].properties, pos: e.lngLat});
+    });
+  }
+  zoomListener() {
+    this._map.on('zoomend', () => {
+      this._zoomLevel = this._map.getZoom();
+      return true;
+    });
+  }
+
+  draw(domElement: HTMLDivElement) {
+    const defaultOpts = {...this.state.viewport, style: this.state.mapStyle, container: domElement};
+    const opts: MapBoxOptions = !this._isOnMobile ?
+      {...defaultOpts, maxBounds: this.state.viewport.bounds} : defaultOpts;
+    this._map = new mapboxgl.Map(opts);
+    this._nav = new mapboxgl.NavigationControl();
+    this._map.addControl(this._nav, 'top-right');
+    this._map.dragRotate.disable();
+    this._map.touchZoomRotate.disableRotation();
+    this.zoomListener();
+  }
   render() {
     const {viewport, mapStyle, token} = this.state;
 
     return (
       <section>
         {process.browser ?
-        (<MapGL
-          {...viewport}
-          mapStyle={mapStyle}
-          onViewportChange={v => this.setState({viewport: v})}
-          mapboxApiAccessToken={token}
-          preventStyleDiffing={false}
-        />) :
+        (<div key={'map-mapbox'} ref={(domElement) => this.draw(domElement)} />) :
         (<MapPlaceholder />)}
       </section>
 
