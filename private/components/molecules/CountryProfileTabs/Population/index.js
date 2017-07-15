d4e6c673291c7dcc768ca6c1d4894df8b44b2914
kraiba <krabz01@gmail.com>
2017-07-15 12:04:12 +0300

    add data from graphql

--- private/components/molecules/CountryProfileTabs/Population/index.js
+++ private/components/molecules/CountryProfileTabs/Population/index.js
@@ -1,42 +1,46 @@
+// @flow
 import { Container, Header, Grid, Icon, Button } from 'semantic-ui-react';
 import React from 'react';
 
-const Population = () => (
-  <Grid>
-    <Grid.Column computer={5} tablet={16} mobile={16}>
-      <Header
-        textAlign="center"
-        as="h3"
-      >
-        WHAT IS THE POPULATION
-      </Header>
-      <Header
-        textAlign="center"
-        as="h1"
-        color="red"
-      >
-        39m
-      </Header>
-    </Grid.Column>
-    <Grid.Column computer={5} tablet={16} mobile={16}>
-      <Header
-        textAlign="center"
-        as="h3"
-      >
-        WHAT IS THE URBAN VS RURAL SPLIT?
-      </Header>
+const Population = (props: TabDataQuery) => {
+  if (!props.populationTab) return new Error('No Population data');
+  return (
+    <Grid>
+      <Grid.Column computer={5} tablet={16} mobile={16}>
+        <Header
+          textAlign="center"
+          as="h3"
+        >
+          WHAT IS THE POPULATION
+        </Header>
+        <Header
+          textAlign="center"
+          as="h1"
+          color="red"
+        >
+          {props.populationTab.population}
+        </Header>
+      </Grid.Column>
+      <Grid.Column computer={5} tablet={16} mobile={16}>
+        <Header
+          textAlign="center"
+          as="h3"
+        >
+          WHAT IS THE URBAN VS RURAL SPLIT?
+        </Header>
 
-    </Grid.Column>
+      </Grid.Column>
 
-    <Grid.Column computer={5} tablet={16} mobile={16}>
-      <Header
-        textAlign="center"
-        as="h3"
-      >
-        WHAT IS THE AGE PROFILE?
-      </Header>
-    </Grid.Column>
-  </Grid>
-);
+      <Grid.Column computer={5} tablet={16} mobile={16}>
+        <Header
+          textAlign="center"
+          as="h3"
+        >
+          WHAT IS THE AGE PROFILE?
+        </Header>
+      </Grid.Column>
+    </Grid>
+  );
+};
 
 export default Population;
