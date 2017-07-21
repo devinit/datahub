d2f187ed1450fb3eca47b36ce0166878758f19a2
Allan Lukwago <epicallan.al@gmail.com>
2017-07-21 15:18:45 +0300

    adding missing map components

---
+++ private/components/molecules/Map/data.js
@@ -0,0 +1,99 @@
+const data = {
+  countryRankings: {
+    flags: true,
+    top: [
+      {number: 1, name: 'India', flag: 'in', value: '490,048,500'},
+      {number: 2, name: 'Nigeria', flag: 'bg', value: '112,955,152'},
+      {number: 3, name: 'China', flag: 'cn', value: '83,750,346'},
+      {number: 4, name: 'Indonesia', flag: 'id', value: '67.842,900'},
+      {number: 5, name: 'DRC', flag: 'cd', value: '62,030,250'},
+      {number: 6, name: 'Bangladesh', flag: 'bd', value: '49,772,572'},
+      {number: 7, name: 'Pakistan', flag: 'pk', value: '44,192.241'},
+      {number: 8, name: 'Ethiopia', flag: 'et', value: '40,528,416'},
+      {number: 9, name: 'Tanzania', flag: 'tz', value: '32,144,442'},
+      {number: 10, name: 'Philippines', flag: 'ph', value: '24,733,995'}
+    ],
+    bottom: [
+      {number: 121, name: 'Poland', flag: 'pl', value: '51,126'},
+      {number: 122, name: 'Montenegro', flag: 'me', value: '11,966'},
+      {number: 123, name: 'Bosnia & Herzegovina', flag: 'ba', value: '10,314'},
+      {number: 124, name: 'Samoa', flag: 'ws', value: '8,987'},
+      {number: 125, name: 'Trinidad & Tobago', flag: 'tt', value: '7,560'},
+      {number: 126, name: 'Tonga', flag: 'to', value: '4,400'},
+      {number: 127, name: 'Seychelles', flag: 'sc', value: '1,368'},
+      {number: 128, name: 'Tuvalu', flag: 'tv', value: '640'},
+      {number: 129, name: 'Slovenia', flag: 'sv', value: '618'},
+      {number: 130, name: 'Belarus', flag: 'by', value: '0'}
+    ]
+  },
+  tabOptions: {
+    dataRevolution: [
+      {key: 0, value: 'Latest (planned) population census'},
+      {key: 1, value: 'Number of poverty surveys in past 10 years'},
+      {key: 2, value: 'Latest (planned) agricultural census'},
+      {key: 3, value: 'Vital statistics from civil registration (births)'},
+      {key: 4, value: 'Vital statistics from civil registration (deaths)'},
+      {key: 5, value: 'Health management information system'},
+      {key: 6, value: 'Education management information system'},
+      {key: 7, value: 'Statistical capacity'},
+    ],
+    forwardLooking: [
+      {key: 0, value: 'UK, DFID planned budgets'},
+      {key: 1, value: 'Canada, Global Affairs planned budgets'},
+      {key: 2, value: 'Denmark, DANIDA and Foreign Affairs planned budgets'},
+      {key: 3, value: 'Netherlands, Foreign Affairs planned budgets'},
+      {key: 4, value: 'Belgian Development Cooperation planned budgets'},
+    ],
+    governmentFinance: [
+      {key: 0, value: 'Government revenue per person (including grants), 2015 PPP$'},
+      {key: 1, value: 'Government revenue (excluding grants), % of GDPs'},
+      {key: 2, value: 'Government revenue (including grants), % of GDP'},
+      {key: 3, value: 'Grants, % of government revenue'},
+      {key: 4, value: 'Government spending per person, 2015 PPP$'},
+    ],
+    humanitarianFinance: [
+      {key: 0, value: 'Humanitarian assistance'},
+    ],
+    internationalFinance: [
+      {key: 0, value: 'Largest international resource flow'},
+      {key: 1, value: 'Total international resource flows, % of GDP'},
+      {key: 2, value: 'ODA, % of international resource flows'},
+      {key: 3, value: 'Foreign direct investment, % of GDP'},
+      {key: 4, value: 'Foreign direct investment per person, US$'},
+      {key: 5, value: 'Remittances received per person, US$'},
+    ],
+    internationalOfficial: [
+      {key: 0, value: 'Gross ODA received, US$'},
+      {key: 1, value: 'Net ODA received, US$'},
+      {key: 2, value: 'Net ODA provided by OECD DAC members, US$'},
+      {key: 3, value: 'ODA per person living on under $1.90 a day, US$'},
+      {key: 4, value: 'Net ODA provided by DAC members, % of GNI'},
+      {key: 5, value: 'Gross ODA provided by DAC members to LDCs, % of GNI'},
+      {key: 6, value: 'Gross other official flows received, US$'},
+      {key: 8, value: 'Net other official flows provided, US$'},
+      {key: 9, value: 'Net south-south development cooperation provided, US$'},
+      {key: 10, value: 'South-south development cooperation provided, % of GNI'},
+    ],
+    poverty: [
+      {key: 0, value: 'Number of people living in extreme poverty'},
+      {key: 1, value: '% of population living in extreme poverty'},
+      {key: 2, value: 'Depth of extreme poverty (poverty gap)'},
+      {key: 3, value: '% of population living on less than $3.10 a day'},
+      {key: 4, value: 'Number of people in the P20'},
+      {key: 5, value: '% of people in the P20'},
+      {key: 6, value: '% of people in the P20 (subnational level)'},
+      {key: 7, value: '% of people in the P20 (survey level)'},
+    ],
+    vulnerability: [
+      {key: 0, value: 'Fragile states'},
+      {key: 1, value: 'Climate vulnerability'},
+      {key: 2, value: 'Human hazard and exposure score'},
+      {key: 3, value: 'Natural hazard and exposure scores'},
+      {key: 4, value: 'Number of UN-coordinated appeals'},
+      {key: 5, value: 'Number of appearances on ECHOs Forgotten Crisis Assessment index'},
+      {key: 6, value: 'Number of people displaced due to human hazards by hosting country'},
+    ]
+  }
+};
+
+export default data;
