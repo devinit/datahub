58d2a8dfc1cbd045d10a6e6ed96c53be5f9036e6
kraiba <krabz01@gmail.com>
2017-07-28 13:31:25 +0300

    add front video

--- private/components/atoms/Backgrounds/index.js
+++ private/components/atoms/Backgrounds/index.js
@@ -4,8 +4,8 @@ import {lightBlack} from 'components/theme/semantic';
 
 export const LightBg = glamorous.div({
   background: '#e9e7e8',
-  paddingTop: '4em',
-  paddingBottom: '4em',
+  paddingTop: '2em',
+  paddingBottom: '2em',
 });
 export const DarkBg = glamorous.div({
   background: lightBlack,
