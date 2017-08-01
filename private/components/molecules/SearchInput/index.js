6430720410512bb094fc1c48cd8ab86c15df7b23
kraiba <krabz01@gmail.com>
2017-08-01 11:44:38 +0300

    fix by removing list class name

--- private/components/molecules/SearchInput/index.js
+++ private/components/molecules/SearchInput/index.js
@@ -93,7 +93,7 @@ class SearchInput extends React.Component {
             onChange={(e) => this.onChange(e.target.value)}
             onKeyDown={(e) => this.onKeyDown(e)}
           />
-          <Div position="relative" className="list">
+          <Div position="relative">
             <List >
               { this.state.countries ?
                   this.state.countries
