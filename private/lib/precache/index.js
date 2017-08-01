46a29a362748a175b17d60acd233e3f2fcc33f70
Allan Lukwago <epicallan.al@gmail.com>
2017-08-01 16:33:58 +0300

    precache improvements and new country data

---
+++ private/lib/precache/index.js
@@ -0,0 +1,25 @@
+// server precache module
+const countriesData = require('../../components/organisms/CountrySearchInput/data');
+const fetch = require('isomorphic-fetch');
+const pagesToPreCache = require('./pages');
+
+const PORT = process.env.PORT || 4444;
+
+const preCache = () => {
+  const homeLink = `http://localhost:${PORT}`;
+  const countrySlugs = countriesData.countries.map(country => `/country?id=${country.id}`);
+  // for batch precaching of country pages
+  const preCacheList = pagesToPreCache.concat(countrySlugs);
+  preCacheList.forEach((link, index) => {
+    setTimeout(() => {
+      fetch(`${homeLink}${link}`).then(response => {
+        if (response.status === 200) return console.info(`${link} was found and is now cached`);
+        return console.error(`${link} was not found or bad response`);
+      })
+      .catch((error) => console.error(error.message));
+    }, 5000);
+  });
+};
+
+preCache();
+
---
+++ private/lib/precache/pages.js
@@ -0,0 +1,1 @@
+module.exports = ['/', '/spotlight-on-uganda', '/unbundling-aid'];
