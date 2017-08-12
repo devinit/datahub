92fc3fbc5ba854b2a8fc366c6affcde74a66aa11
Allan Lukwago <epicallan.al@gmail.com>
2017-08-12 17:54:37 +0300

    page text integration country profile tabs

---
+++ private/components/organisms/PagesData/data.js
@@ -0,0 +1,195 @@
+/* eslint-disable */
+// this file is auto generated
+
+module.exports = {
+  countryProfile: [
+    {
+      id: 'introduction',
+      title:
+        'Explore this in-depth profile of {country} to find out overall levels of poverty, income distribution, division of wealth and more. Discover how national and sub-national revenue is generated',
+      narrative: ''
+    },
+    {
+      id: 'overview-ctry-poorest-people',
+      title: 'HOW MANY OF THE POOREST PEOPLE GLOBALLY LIVE IN {country}?',
+      narrative: ''
+    },
+    {
+      id: 'overview-ctry-resources',
+      title: 'WHAT RESOURCES ARE AVAILABLE?',
+      narrative: ''
+    },
+    {
+      id: 'overview-govt-spentperperson',
+      title: 'HOW MUCH DOES THE GOVERNMENT SPEND PER PERSON?',
+      narrative: ''
+    },
+    {
+      id: 'poverty-reduction-ctry',
+      title: 'IS POVERTY REDUCING OVER TIME?',
+      narrative: ''
+    },
+    { id: 'poverty-depth-ctry', title: 'HOW DEEP IS POVERTY?', narrative: '' },
+    {
+      id: 'income-distribution-ctry',
+      title: 'HOW IS INCOME DISTRIBUTED?',
+      narrative: ''
+    },
+    { id: 'popn-ctry', title: 'WHAT IS THE POPULATION?', narrative: '' },
+    {
+      id: 'popn-distribution',
+      title: 'WHAT IS THE URBAN VS RURAL SPLIT?',
+      narrative: ''
+    },
+    {
+      id: 'popn-distribution-age-ctry',
+      title: 'WHAT IS THE AGE PROFILE?',
+      narrative: ''
+    },
+    {
+      id: 'totalrevenue-ctry',
+      title: 'WHAT IS THE TOTAL REVENUE OF {country}?',
+      narrative: ''
+    },
+    {
+      id: 'govt-revenue-external',
+      title: 'HOW MUCH GOVERNMENT REVENUE COMES FROM EXTERNAL GRANTS (AID)?',
+      narrative: ''
+    },
+    {
+      id: 'ctry-expenditure',
+      title: 'HOW IS SPENDING ALLOCATED?',
+      narrative: 'Capital Expenditure'
+    },
+    {
+      id: 'diff-providers-priorities',
+      title: 'Different providers, different priorities',
+      narrative:
+        'How effectively are donors targeting the countries with the highest numbers of people in poverty and the least domestic public resources to address it? This chart allows you to select a provider of aid to see how it allocates aid across countries'
+    },
+    {
+      id: 'resources-able-to-meet-needs',
+      title:
+        'Are domestic public resources able to meet the needs of the poorest people?',
+      narrative:
+        'The proportion of people living in extreme poverty worldwide has more than halved since 2000, yet progress is varied across countries. Although domestic public resources are an important resource to drive efforts to end poverty, they remain scarce in many countries where poverty is high'
+    },
+    {
+      id: 'Unbundling aid',
+      title: 'Unbundling aid',
+      narrative:
+        'Explore and compare funding priorities for official development assistance'
+    },
+    {
+      id: 'aid-allocated-ctry',
+      title: 'AS A SHARE OF GNI, HOW MUCH AID IS ALLOCATED TO {country}?',
+      narrative: ''
+    },
+    {
+      id: 'mixture-of-resources',
+      title: 'WHAT’S THE MIX OF RESOURCES?',
+      narrative: ''
+    },
+    {
+      id: 'Unbundling aid',
+      title: 'Unbundling aid',
+      narrative:
+        'Explore and compare funding priorities for official development assistance  Using this visualisation'
+    }
+  ],
+  spotlightDistrict: [
+    {
+      id: 'introduction',
+      title:
+        'Explore this in-depth profile of {country} to find out overall levels of poverty, income distribution, division of wealth and more. Discover how national and sub-national revenue is generated',
+      narrative: ''
+    },
+    {
+      id: 'overview-ctry-poorest-people',
+      title: 'HOW MANY OF THE POOREST PEOPLE GLOBALLY LIVE IN {country}?',
+      narrative: ''
+    },
+    {
+      id: 'overview-ctry-resources',
+      title: 'WHAT RESOURCES ARE AVAILABLE?',
+      narrative: ''
+    },
+    {
+      id: 'overview-govt-spentperperson',
+      title: 'HOW MUCH DOES THE GOVERNMENT SPEND PER PERSON?',
+      narrative: ''
+    },
+    {
+      id: 'poverty-reduction-ctry',
+      title: 'IS POVERTY REDUCING OVER TIME?',
+      narrative: ''
+    },
+    { id: 'poverty-depth-ctry', title: 'HOW DEEP IS POVERTY?', narrative: '' },
+    {
+      id: 'income-distribution-ctry',
+      title: 'HOW IS INCOME DISTRIBUTED?',
+      narrative: ''
+    },
+    { id: 'popn-ctry', title: 'WHAT IS THE POPULATION?', narrative: '' },
+    {
+      id: 'popn-distribution',
+      title: 'WHAT IS THE URBAN VS RURAL SPLIT?',
+      narrative: ''
+    },
+    {
+      id: 'popn-distribution-age-ctry',
+      title: 'WHAT IS THE AGE PROFILE?',
+      narrative: ''
+    },
+    {
+      id: 'totalrevenue-ctry',
+      title: 'WHAT IS THE TOTAL REVENUE OF CTRY?',
+      narrative: ''
+    },
+    {
+      id: 'govt-revenue-external',
+      title: 'HOW MUCH GOVERNMENT REVENUE COMES FROM EXTERNAL GRANTS (AID)?',
+      narrative: ''
+    },
+    {
+      id: 'ctry-expenditure',
+      title: 'HOW IS SPENDING ALLOCATED?',
+      narrative: 'Capital Expenditure'
+    },
+    {
+      id: 'diff-providers-priorities',
+      title: 'Different providers, different priorities',
+      narrative:
+        'How effectively are donors targeting the countries with the highest numbers of people in poverty and the least domestic public resources to address it? This chart allows you to select a provider of aid to see how it allocates aid across countries'
+    },
+    {
+      id: 'resources-able-to-meet-needs',
+      title:
+        'Are domestic public resources able to meet the needs of the poorest people?',
+      narrative:
+        'The proportion of people living in extreme poverty worldwide has more than halved since 2000, yet progress is varied across countries. Although domestic public resources are an important resource to drive efforts to end poverty, they remain scarce in many countries where poverty is high'
+    },
+    {
+      id: 'Unbundling aid',
+      title: 'Unbundling aid',
+      narrative:
+        'Explore and compare funding priorities for official development assistance'
+    },
+    {
+      id: 'aid-allocated-ctry',
+      title: 'AS A SHARE OF GNI, HOW MUCH AID IS ALLOCATED TO Country?',
+      narrative: ''
+    },
+    {
+      id: 'mixture-of-resources',
+      title: 'WHAT’S THE MIX OF RESOURCES?',
+      narrative: ''
+    },
+    {
+      id: 'Unbundling aid',
+      title: 'Unbundling aid',
+      narrative:
+        'Explore and compare funding priorities for official development assistance  Using this visualisation'
+    }
+  ]
+};
---
+++ private/components/organisms/PagesData/index.js
@@ -0,0 +1,43 @@
+// @flow
+/**
+ * exports out page data for various pages after doing any necessary modifications
+ */
+import {getCountryName} from 'lib/utils';
+import data from './data';
+
+const pagesData = (data: PageDataQuery);
+
+export type PageUnit = {|
+  id: ?string,
+  title: ?string,
+  narrative: ?string,
+|}
+type ReplaceFieldsArgs = {
+  pageData: PageUnit[],
+  toReplace: string,
+  replacement: string
+}
+const replaceFields = (args: ReplaceFieldsArgs): PageUnit[] => {
+  const {pageData, toReplace, replacement} = args;
+  return pageData.map((obj: PageUnit) => {
+    const title = obj.title && obj.title.includes(toReplace) ?
+      obj.title.replace(toReplace, replacement) : obj.title;
+    const narrative = obj.narrative && obj.narrative.includes(toReplace) ?
+      obj.narrative.replace(toReplace, replacement) : obj.narrative;
+    return {id: obj.id, title, narrative};
+  });
+};
+
+export const getCountryProfileData = (slug: string): PageUnit[] => {
+  const countryName = getCountryName(slug);
+  if (!pagesData.countryProfile) throw new Error('country profile page data missing');
+  const pageData: PageUnit[] = pagesData.countryProfile;
+  return replaceFields({pageData, toReplace: '{country}', replacement: countryName});
+};
+
+export const getPageUnitById = (data: PageUnit[]) =>
+  (id: string): PageUnit => {
+    const pageUnit: PageUnit | void = data.find(obj => obj.id === id);
+    if (!pageUnit) return { title: 'Error getting title', narrative: 'Error getting narrative for', id};
+    return pageUnit;
+  };
