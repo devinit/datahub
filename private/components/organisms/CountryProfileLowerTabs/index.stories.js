d719cf0427baa44be74fe362ad101e595ffb8a3f
Allan Lukwago <epicallan.al@gmail.com>
2017-08-13 18:49:06 +0300

    snapshots / tests changes

--- private/components/organisms/CountryProfileLowerTabs/index.stories.js
+++ private/components/organisms/CountryProfileLowerTabs/index.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import withApolloProvider from 'lib/storybook-addon/apollo';
 import CountryProfileLowerTabs from '.';
 
-storiesOf('Tabs', module)
+storiesOf('Tabs DontTest', module)
   .addDecorator(withApolloProvider())
   .add('Country Profiles Lower tabs uganda', () => <CountryProfileLowerTabs id={'uganda'} />)
   .add('Country Profiles Lower tabs rwanda', () => <CountryProfileLowerTabs id={'rwanda'} />)
