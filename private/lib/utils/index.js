d35328417b7ff4a2812e9ff8098344b01e524e8b
Allan Lukwago <epicallan.al@gmail.com>
2017-08-14 15:01:22 +0300

    spotlight on uganda tabs narrative WIP

--- private/lib/utils/index.js
+++ private/lib/utils/index.js
@@ -1,8 +1,17 @@
 // @flow
 import countriesData from 'components/organisms/CountrySearchInput/data';
+import ugDistrictData from 'components/organisms/CountrySearchInput/ug-data';
 
 export const getCountryName = (slug: string): string => {
   const country = countriesData.countries.find(country => country.slug === slug);
   if (country && country.name) return country.name;
+  return slug;
+};
+
+export const getDistrictName = (slug: string, country: string): string => {
+  // TODO: handle spotlight kenya
+  if (country !== 'uganda') throw new Error('we are only dealing with spotlight uganda for now');
+  const district = ugDistrictData.districts.find(district => district.slug === slug);
+  if (district && district.name) return district.name;
   return slug;
 };
