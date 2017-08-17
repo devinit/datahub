ba16c02b39c99d103a9f068abc61afbb0508a8ab
kraiba <krabz01@gmail.com>
2017-08-17 09:21:56 +0300

    add print titles

--- private/components/molecules/CountryProfileTabs/GovernmentFinanceLower/index.js
+++ private/components/molecules/CountryProfileTabs/GovernmentFinanceLower/index.js
@@ -1,8 +1,9 @@
 // @flow
-import { Container, Header, Grid, Divider } from 'semantic-ui-react';
+import { Container, Header, Grid, Divider, Segment } from 'semantic-ui-react';
 import {Div} from 'glamorous';
 import React from 'react';
 import {Lead} from 'components/atoms/BodyText';
+import {PrintContainer} from 'components/atoms/Container';
 import type {PageUnit} from 'components/organisms/PagesData';
 import {getPageUnitById} from 'components/organisms/PagesData';
 
@@ -34,6 +35,16 @@ const Government = (props: Props) => {
     <Divider />
     <Div paddingTop="2em" paddingBottom="2em" >
       <Container id="print-chart">
+        <PrintContainer>
+          <Segment textAlign="center" vertical>
+            <img src="/img/print-logo.jpg" alt="Development Initiatives" height="50" width="132" />
+            <Header>
+              <Header.Content as="h2">Domestic public resources in Uganda</Header.Content>
+              <Header.Subheader as="h3">www.devinit.org</Header.Subheader>
+            </Header>
+            <Header as="h2">Government revenue, financing and expenditure</Header>
+          </Segment>
+        </PrintContainer>
         {props.children}
       </Container>
     </Div>
