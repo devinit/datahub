de132d21f7648f1868727440319da2fe88646ee0
Ernest Okot <okot08@gmail.com>
2017-08-21 04:34:59 +0300

    fixes gov't finance line chart bug

--- private/components/molecules/TripleLinePartition/index.js
+++ private/components/molecules/TripleLinePartition/index.js
@@ -1,17 +1,17 @@
 // @flow
-import React, {Component} from 'react';
-import glamorous from 'glamorous';
-import { Dropdown, Grid, Label, Segment, Container, Header } from 'semantic-ui-react';
-import { SectionHeader } from 'components/atoms/Header';
-import { makeUnique } from '@devinit/charts/lib/factories/createDataset';
-import LoadingBar from 'components/molecules/LoadingBar';
-import ExportChart from 'components/molecules/ExportChart';
-import TreeChart from 'components/atoms/TreeChart/index';
-import Timeline from 'components/atoms/Timeline/index';
-import { LightBg } from 'components/atoms/Backgrounds';
-import TourContainer from 'components/molecules/TourContainer';
-import {PrintContainer} from 'components/atoms/Container';
-import GovernmentFinanceTour from 'components/atoms/GovernmentFinanceTour';
+import React, {Component} from "react";
+import glamorous from "glamorous";
+import {Container, Dropdown, Grid, Header, Label, Segment} from "semantic-ui-react";
+import {SectionHeader} from "components/atoms/Header";
+import {makeUnique} from "@devinit/charts/lib/factories/createDataset";
+import LoadingBar from "components/molecules/LoadingBar";
+import ExportChart from "components/molecules/ExportChart";
+import TreeChart from "components/atoms/TreeChart/index";
+import Timeline from "components/atoms/Timeline/index";
+import {LightBg} from "components/atoms/Backgrounds";
+import TourContainer from "components/molecules/TourContainer";
+import {PrintContainer} from "components/atoms/Container";
+import GovernmentFinanceTour from "components/atoms/GovernmentFinanceTour";
 
 type State = {
   year: number,
@@ -33,9 +33,9 @@ type State = {
 type Props = {
   currencyCode: string,
   loading: boolean,
-   // from cached shared state
+  // from cached shared state
   year?: number,
-    // from cached shared state
+  // from cached shared state
   budgetType: string,
   shouldScrollIntoView?: boolean,
   startYear: number,
@@ -87,8 +87,8 @@ export default class GovtRFE extends Component {
   calculateInitialState(props: Props) {
     const year = props.year || props.startYear;
     const currencies = [
-      { text: 'Constant 2015 US$', value: 'US$' },
-      { text: `Current ${props.currencyCode}`, value: props.currencyCode },
+      {text: 'Constant 2015 US$', value: 'US$'},
+      {text: `Current ${props.currencyCode}`, value: props.currencyCode},
     ];
     const currency = 'US$';
     const budgetTypes = this.calculateBudgetTypes(
@@ -100,9 +100,9 @@ export default class GovtRFE extends Component {
     const financeTrend = this.calculateTrend(props.finance, currency);
     const expenditureTrend = this.calculateTrend(props.expenditure, currency);
 
-    const revenueTree = this.calculateTree(revenueTrend, year, budgetType, currency);
-    const financeTree = this.calculateTree(financeTrend, year, budgetType, currency);
-    const expenditureTree = this.calculateTree(expenditureTrend, year, budgetType, currency);
+    const revenueTree = this.calculateTree(props.revenueAndGrants, year, budgetType, currency);
+    const financeTree = this.calculateTree(props.finance, year, budgetType, currency);
+    const expenditureTree = this.calculateTree(props.expenditure, year, budgetType, currency);
 
     return {
       revenueTourVisible: false,
@@ -122,6 +122,7 @@ export default class GovtRFE extends Component {
       expenditureTree,
     };
   }
+
   toogleRevenueTour() {
     if (this.state.revenueTourVisible) {
       this.setState({revenueTourVisible: false});
@@ -129,6 +130,7 @@ export default class GovtRFE extends Component {
       this.setState({revenueTourVisible: true});
     }
   }
+
   setYear(year: number) {
     const budgetTypes = this.calculateBudgetTypes(
       [...this.props.finance, ...this.props.revenueAndGrants, ...this.props.expenditure],
@@ -137,28 +139,29 @@ export default class GovtRFE extends Component {
     const budgetType = budgetTypes[0] && budgetTypes[0].value;
     const currency = this.state.currency;
 
-    const revenueTrend = this.calculateTrend(this.props.revenueAndGrants, currency);
-    const financeTrend = this.calculateTrend(this.props.finance, currency);
-    const expenditureTrend = this.calculateTrend(this.props.expenditure, currency);
-
     this.setState({
       year,
       budgetType,
       budgetTypes,
-      revenueTree: this.calculateTree(revenueTrend, year, budgetType, currency),
-      financeTree: this.calculateTree(financeTrend, year, budgetType, currency),
-      expenditureTree: this.calculateTree(expenditureTrend, year, budgetType, currency),
+      revenueTree: this.calculateTree(this.props.revenueAndGrants, year, budgetType, currency),
+      financeTree: this.calculateTree(this.props.finance, year, budgetType, currency),
+      expenditureTree: this.calculateTree(this.props.expenditure, year, budgetType, currency),
     });
   }
 
   setBudgetType(budgetType: string) {
     const currency = this.state.currency;
     const year = this.state.year;
+    const revenueTree = this.calculateTree(this.props.revenueAndGrants, year, budgetType, currency);
+    const financeTree = this.calculateTree(this.props.finance, year, budgetType, currency);
+    const expenditureTree =
+      this.calculateTree(this.props.expenditure, year, budgetType, currency);
+
     this.setState({
       budgetType,
-      revenueTree: this.calculateTree(this.state.revenueTrend, year, budgetType, currency),
-      financeTree: this.calculateTree(this.state.financeTrend, year, budgetType, currency),
-      expenditureTree: this.calculateTree(this.state.expenditureTrend, year, budgetType, currency),
+      revenueTree,
+      financeTree,
+      expenditureTree,
     });
   }
 
@@ -168,45 +171,49 @@ export default class GovtRFE extends Component {
     const revenueTrend = this.calculateTrend(this.props.revenueAndGrants, currency);
     const financeTrend = this.calculateTrend(this.props.finance, currency);
     const expenditureTrend = this.calculateTrend(this.props.expenditure, currency);
+    const revenueTree = this.calculateTree(this.props.revenueAndGrants, year, budgetType, currency);
+    const financeTree = this.calculateTree(this.props.finance, year, budgetType, currency);
+    const expenditureTree = this.calculateTree(this.props.expenditure, year, budgetType, currency);
+
     this.setState({
       currency,
       budgetType,
       revenueTrend,
       financeTrend,
       expenditureTrend,
-      revenueTree: this.calculateTree(revenueTrend, year, budgetType, currency),
-      financeTree: this.calculateTree(financeTrend, year, budgetType, currency),
-      expenditureTree: this.calculateTree(expenditureTrend, year, budgetType, currency),
+      revenueTree,
+      financeTree,
+      expenditureTree,
     });
   }
 
   setRevenueLevel(revenueLevel: string) {
-    const leveled = this.props.revenueAndGrants.filter(d => {
-      return d.levels.indexOf(revenueLevel) > -1;
-    });
+    const revenueTrend =
+      this.calculateTrend(this.props.revenueAndGrants, this.state.currency, revenueLevel);
+
     this.setState({
       revenueLevel,
-      revenueTrend: this.calculateTrend(leveled, this.state.currency),
+      revenueTrend,
     });
   }
 
   setFinanceLevel(financeLevel: string) {
-    const leveled = this.props.finance.filter(d => {
-      return d.levels.indexOf(financeLevel) > -1;
-    });
+    const financeTrend =
+      this.calculateTrend(this.props.finance, this.state.currency, financeLevel);
+
     this.setState({
       financeLevel,
-      financeTrend: this.calculateTrend(leveled, this.state.currency),
+      financeTrend,
     });
   }
 
   setExpenditureLevel(expenditureLevel: string) {
-    const leveled = this.props.expenditure.filter(d => {
-      return d.levels.indexOf(expenditureLevel) > -1;
-    });
+    const expenditureTrend =
+      this.calculateTrend(this.props.expenditure, this.state.currency, expenditureLevel);
+
     this.setState({
       expenditureLevel,
-      expenditureTrend: this.calculateTrend(leveled, this.state.currency),
+      expenditureTrend,
     });
   }
 
@@ -215,16 +222,26 @@ export default class GovtRFE extends Component {
     return makeUnique(data.filter(d => d.year === year).map(d => d.budget_type))
       .sort()
       .map(text => {
-        return { text: text.slice(0, 1).toUpperCase() + text.slice(1), value: text, key: text };
+        return {text: text.slice(0, 1).toUpperCase() + text.slice(1), value: text, key: text};
       });
   }
 
   // eslint-disable-next-line class-methods-use-this
-  calculateTrend(data: Object[], currency: string = 'US$') {
-    return data.map(d => ({
-      ...d,
-      value: currency === 'US$' ? d.value : d.value_ncu,
-    }));
+  calculateTrend(data: Object[], currency: string = 'US$', level = null) {
+    const trend = data
+      .filter(d => {
+        const isActualOrProjected = d.budget_type.match(/(actual|proj)/gi);
+        const isAtSelectedLevel = level ?
+          d.levels.length - 1 === d.levels.indexOf(level) :
+          d.levels.length === 1;
+
+        return isActualOrProjected && isAtSelectedLevel;
+      })
+      .map(d => ({
+        ...d,
+        value: currency === 'US$' ? d.value : d.value_ncu,
+      }));
+    return trend;
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -244,9 +261,10 @@ export default class GovtRFE extends Component {
         };
       });
   }
+
   render() {
     if (this.props.loading) {
-      return <LoadingBar loading={this.props.loading} />;
+      return <LoadingBar loading={this.props.loading}/>;
     }
     return (
       <LightBg
@@ -257,7 +275,7 @@ export default class GovtRFE extends Component {
             <Container id="print-chart">
               <PrintContainer>
                 <Segment textAlign="center" vertical>
-                  <img src="/img/print-logo.jpg" alt="Development Initiatives" height="50" width="132" />
+                  <img src="/img/print-logo.jpg" alt="Development Initiatives" height="50" width="132"/>
                   <Header>
                     <Header.Content as="h2">Domestic public resources in Uganda</Header.Content>
                     <Header.Subheader as="h3">www.devinit.org</Header.Subheader>
@@ -274,12 +292,12 @@ export default class GovtRFE extends Component {
                   chartId: this.props.chartId
                 }}
               />
-              <Segment basic clearing style={{ paddingRight: 0, paddingLeft: 0 }}>
-                <SectionHeader color="#fff" style={{ float: 'left' }}>
+              <Segment basic clearing style={{paddingRight: 0, paddingLeft: 0}}>
+                <SectionHeader color="#fff" style={{float: 'left'}}>
                   REVENUE AND GRANT <span>{this.state.year}</span>
                 </SectionHeader>
 
-                <Segment basic floated={'right'} style={{ padding: 0, margin: 0 }}>
+                <Segment basic floated={'right'} style={{padding: 0, margin: 0}}>
                   <Label>Budget Type</Label>
                   <Dropdown
                     selection
@@ -299,26 +317,26 @@ export default class GovtRFE extends Component {
               </Segment>
 
               <Grid>
-                <Grid.Column width={5} style={{ paddingRight: 0 }}>
-                  <CardContainer style={{ paddingLeft: '30px' }}>
+                <Grid.Column width={5} style={{paddingRight: 0}}>
+                  <CardContainer style={{paddingLeft: '30px'}}>
                     <Timeline
                       onYearChanged={year => this.setYear(+year)}
                       height="180px"
                       config={{
                         ...this.props.config.line,
-                        anchor: { start: this.state.year.toString() },
+                        anchor: {start: this.state.year.toString()},
                       }}
                       data={this.state.revenueTrend}
                     />
                   </CardContainer>
                 </Grid.Column>
 
-                <Grid.Column width={11} style={{ paddingLeft: 0 }}>
+                <Grid.Column width={11} style={{paddingLeft: 0}}>
                   <TreeChart
                     height="222px"
                     config={{
                       ...this.props.config.partition,
-                      labeling: { prefix: this.state.currency },
+                      labeling: {prefix: this.state.currency},
                     }}
                     onClick={(d: { id: string }) => this.setRevenueLevel(d.id)}
                     data={this.state.revenueTree}
@@ -327,26 +345,26 @@ export default class GovtRFE extends Component {
               </Grid>
 
               <Grid>
-                <Grid.Column width={5} style={{ paddingRight: 0 }}>
-                  <CardContainer style={{ paddingLeft: '30px' }}>
+                <Grid.Column width={5} style={{paddingRight: 0}}>
+                  <CardContainer style={{paddingLeft: '30px'}}>
                     <Timeline
                       onYearChanged={year => this.setYear(+year)}
                       height="180px"
                       config={{
                         ...this.props.config.line,
-                        anchor: { start: this.state.year.toString() },
+                        anchor: {start: this.state.year.toString()},
                       }}
                       data={this.state.financeTrend}
                     />
                   </CardContainer>
                 </Grid.Column>
 
-                <Grid.Column width={11} style={{ paddingLeft: 0 }}>
+                <Grid.Column width={11} style={{paddingLeft: 0}}>
                   <TreeChart
                     height="222px"
                     config={{
                       ...this.props.config.partition,
-                      labeling: { prefix: this.state.currency },
+                      labeling: {prefix: this.state.currency},
                     }}
                     onClick={(d: { id: string }) => this.setFinanceLevel(d.id)}
                     data={this.state.financeTree}
@@ -370,12 +388,12 @@ export default class GovtRFE extends Component {
             </HeadingContainer>
           </Container>
           <Container>
-            <Segment basic clearing style={{ paddingRight: 0, paddingLeft: 0 }}>
-              <SectionHeader color="#fff" style={{ float: 'left' }}>
+            <Segment basic clearing style={{paddingRight: 0, paddingLeft: 0}}>
+              <SectionHeader color="#fff" style={{float: 'left'}}>
                 EXPENDITURE <span>{this.state.year}</span>
               </SectionHeader>
 
-              <Segment basic floated={'right'} style={{ padding: 0, margin: 0 }}>
+              <Segment basic floated={'right'} style={{padding: 0, margin: 0}}>
                 <Label>Budget Type</Label>
                 <Dropdown
                   selection
@@ -395,26 +413,26 @@ export default class GovtRFE extends Component {
             </Segment>
 
             <Grid>
-              <Grid.Column width={5} style={{ paddingRight: 0 }}>
-                <CardContainer style={{ paddingLeft: '30px' }}>
+              <Grid.Column width={5} style={{paddingRight: 0}}>
+                <CardContainer style={{paddingLeft: '30px'}}>
                   <Timeline
                     onYearChanged={year => this.setYear(+year)}
                     height="250px"
                     config={{
                       ...this.props.config.line,
-                      anchor: { start: this.state.year.toString() },
+                      anchor: {start: this.state.year.toString()},
                     }}
                     data={this.state.expenditureTrend}
                   />
                 </CardContainer>
               </Grid.Column>
 
-              <Grid.Column width={11} style={{ paddingLeft: 0 }}>
+              <Grid.Column width={11} style={{paddingLeft: 0}}>
                 <TreeChart
                   height="292px"
                   config={{
                     ...this.props.config.partition,
-                    labeling: { prefix: this.state.currency },
+                    labeling: {prefix: this.state.currency},
                   }}
                   onClick={(d: { id: string }) => this.setExpenditureLevel(d.id)}
                   data={this.state.expenditureTree}
