340bb2d7e19feb003c171f6225e7cfdd3b7befd4
Allan Lukwago <epicallan.al@gmail.com>
2017-09-11 15:42:19 +0300

    minor story title change

--- private/components/organisms/InternationalResourcesChart/internationalResourcesChart.stories.js
+++ private/components/organisms/InternationalResourcesChart/internationalResourcesChart.stories.js
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react';
 import withApolloProvider from 'lib/storybook-addon/apollo';
 import InternationalResourcesChart from '.';
 
-storiesOf('Country Profile Charts', module)
+storiesOf('Tabs', module)
   .addDecorator(withApolloProvider())
   .add('International Resources', () => <InternationalResourcesChart id={'uganda'} />);
