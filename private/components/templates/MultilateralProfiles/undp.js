02c670af6938f7664c259ab7f5cd9a8e5e9b542c
Allan Lukwago <epicallan.al@gmail.com>
2017-09-12 19:35:29 +0300

    test pages for multilateral profiles

---
+++ private/components/templates/MultilateralProfiles/undp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () =>
+  (<iframe
+    src="http://data.devinit.org/#!/multilateral/undp"
+    title="UNDP"
+    frameBorder="0"
+    style={{ width: '100%', height: '2000px' }}
+  />);
