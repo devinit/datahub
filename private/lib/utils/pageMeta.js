4a886e9b98b31f31297b434302e209059cae1838
Allan Lukwago <epicallan.al@gmail.com>
2017-09-13 12:48:09 +0300

    page mata work in progress

---
+++ private/lib/utils/pageMeta.js
@@ -0,0 +1,43 @@
+// @flow
+import {getCountryName, getDistrictName} from '.';
+
+export type PageMetaArgs = {
+  query: string,
+  pathname: string
+}
+export type PageMeta = {
+  title: string,
+  image?: string,
+  width?: string,
+  height?: string,
+}
+export type MetaMapping = {
+  [pathname: string]: PageMeta
+}
+const metaMapping = (args: PageMetaArgs): MetaMapping => ({
+  '/': {
+    title: 'Development Data Hub',
+    image: '',
+    width: '',
+    height: '',
+  },
+  country: {
+    title: getCountryName(args.query),
+    image: '',
+    width: '',
+    height: '',
+  },
+  uganda: {
+    title: getDistrictName(args.query, 'uganda'),
+    image: '',
+    width: '',
+    height: '',
+  }
+});
+
+const getPageMeta = (args: PageMetaArgs): PageMeta => {
+  const mapping = metaMapping(args);
+  return mapping[args.pathname] || {title: 'Development Data Hub'};
+};
+
+export default getPageMeta;
