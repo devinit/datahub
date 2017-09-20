de829c402b3d7307514982db47f3e4c06180071f
Ernest Okot <okot08@gmail.com>
2017-09-20 13:16:10 +0300

    updated partition header for responsiveness

--- private/components/atoms/LinePartitionHeader/index.js
+++ private/components/atoms/LinePartitionHeader/index.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { Dropdown, Label, Segment } from 'semantic-ui-react';
+import { Grid, Dropdown, Label, Segment } from 'semantic-ui-react';
 import { SectionHeader } from 'components/atoms/Header/index';
 
 type Props = {
@@ -15,29 +15,32 @@ type Props = {
 };
 
 const LinePartitionHeader = (props: Props) => {
-  return (<Segment basic clearing style={{ paddingRight: 0, paddingLeft: 0 }}>
-    <SectionHeader color="#fff" style={{ float: 'left' }}>
-      {props.title} <span>{props.year}</span>
-    </SectionHeader>
-
-    <Segment basic floated={'right'} style={{ padding: 0, margin: 0 }}>
-      <Label style={{background: 'transparent'}}>Budget Type</Label>
-      <Dropdown
-        selection
-        value={props.budgetType}
-        options={props.budgetTypeOptions}
-        onChange={(e, data) => props.onChangeBudgetType(data.value)}
-      />
-      <Label style={{background: 'transparent'}}>Currency</Label>
-      <Dropdown
-        compact
-        selection
-        value={props.currency}
-        options={props.currencyOptions}
-        onChange={(e, data) => props.onChangeCurrency(data.value)}
-      />
-    </Segment>
-  </Segment>);
+  return (<Grid>
+    <Grid.Column width={16} style={{ paddingLeft: 0, paddingRight: 0 }}>
+      <Segment basic clearing style={{ paddingRight: 0, paddingLeft: 0 }}>
+        <SectionHeader color="#fff" style={{ float: 'left' }}>
+          {props.title} <span>{props.year}</span>
+        </SectionHeader>
+        <Segment basic floated={'right'} style={{ padding: 0, margin: 0 }}>
+          <Label style={{background: 'transparent'}}>Budget Type</Label>
+          <Dropdown
+            selection
+            value={props.budgetType}
+            options={props.budgetTypeOptions}
+            onChange={(e, data) => props.onChangeBudgetType(data.value)}
+          />
+          <Label style={{background: 'transparent'}}>Currency</Label>
+          <Dropdown
+            compact
+            selection
+            value={props.currency}
+            options={props.currencyOptions}
+            onChange={(e, data) => props.onChangeCurrency(data.value)}
+          />
+        </Segment>
+      </Segment>
+    </Grid.Column>
+  </Grid>);
 };
 
 export default LinePartitionHeader;
