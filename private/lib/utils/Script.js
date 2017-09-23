f4afb340559c63a69c82c61867d37c720e6fc712
Allan Lukwago <epicallan.al@gmail.com>
2017-09-23 19:04:20 +0300

    putback deleted script

---
+++ private/lib/utils/Script.js
@@ -0,0 +1,7 @@
+// @flow
+/* eslint-disable */
+type Props = {
+  children: any,
+};
+export default (props: Props) =>
+  <script dangerouslySetInnerHTML={{ __html: `(${props.children.toString()})();` }} />;
