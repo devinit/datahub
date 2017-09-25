cbc221acf371675a06abeb1b8a191eff7991edc1
Allan Lukwago <epicallan.al@gmail.com>
2017-09-25 09:53:10 +0300

    remove test treemap files

--- private/components/atoms/Treemap/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import data from './data';
-import Treemap from '.';
-
-storiesOf('Treemap', module).add('treemap', () =>
-  <Treemap data={data} />,
-);
