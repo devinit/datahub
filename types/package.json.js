c3216d220df14b296a5d29c6354b999c970a79dd
Allan Lukwago <epicallan.al@gmail.com>
2017-09-29 09:17:56 +0300

    remove unnecssary api

--- types/package.json.js
+++ types/package.json.js
@@ -3,7 +3,6 @@ declare module '../../package.json' {
   declare var version: string
   declare var config: {
     api: string,
-    old_datahub: string,
-    old_datahub_api: string
+    old_datahub: string
   };
 }
