8324e8aa649807615d6b0ad88b1aa99354e6bb5f
aleku399 <aleku399@gmail.com>
2017-10-22 16:14:04 +0300

    dropdown refactor

---
+++ private/components/molecules/NavigationBarTabsItems/styles.js
@@ -0,0 +1,47 @@
+import glamorous from 'glamorous';
+import { lightBlack, white } from 'components/theme/semantic';
+
+export const SelectWrapper = glamorous.div({
+  display: 'inline-block',
+  '& .menu .text': {
+    paddingLeft: '0.5em'
+  },
+  '& .selected :before': {
+    content: '"✓ "',
+    marginLeft: '-1em'
+  },
+  '& .ui.dropdown .menu>.item': {
+    fontWeight: '500',
+    fontSize: '1.1em'
+  },
+  '& .ui.selection, .ui.selection.dropdown, .ui.selection.dropdown:hover': {
+    background: lightBlack,
+    border: 'none',
+  },
+  '& .ui div:first-child, .ui.selection.visible.dropdown>.text:not(.default), .dropdown.icon': {
+    color: white,
+    fontWeight: 700,
+    fontSize: '18px'
+  }
+});
+
+export const Wrapper = glamorous.div({
+  textAlign: 'center',
+  paddingTop: '.5em',
+  paddingBottom: '.5em',
+  background: lightBlack,
+  '& .ui.selection.active.dropdown .menu': {
+    borderRadius: '0.25em',
+    marginTop: '-2em'
+  },
+  '& .ui.button': {
+    fontWeight: '500',
+    paddingTop: '.5em',
+    paddingBottom: '.5em',
+    paddingRight: '.85em',
+    paddingLeft: '0.75em',
+  },
+  '& i.icon': {
+    color: white
+  },
+});
