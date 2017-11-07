a4ab777b1e002bef4dc3570d1b4e521d21b04557
epicallan <epicallan.al@gmail.com>
2017-11-07 12:30:10 +0300

    spotlights && navbar tabs & spotlight maps work

---
+++ private/components/organisms/NavBarTabs/spotlightUg.js
@@ -0,0 +1,41 @@
+// @flow
+import React from 'react';
+import { changeSpotlightIndicatorUg, changeLoadingStatus} from 'lib/actions';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import type { SpotLightlIndicator} from 'lib/actions';
+import type { State, Action } from 'lib/reducers';
+import type { Props } from 'components/molecules/NavigationBarTabs';
+import NavigationBarTabs from 'components/molecules/NavigationBarTabs';
+import type {StateToShare} from 'components/molecules/ChartShare';
+import type {BoundAction, BoundState} from './types';
+import data from './uganda';
+
+const mapDispatchToProps = (dispatch: Dispatch<Action>): BoundAction<SpotLightlIndicator> =>
+  ({
+    changeActiveIndicator: bindActionCreators(changeSpotlightIndicatorUg, dispatch),
+    changeLoadingStatus: bindActionCreators(changeLoadingStatus, dispatch),
+  });
+
+const mapStateToProps = ({ app }: State): BoundState =>
+  ({ activeIndicator: app.indicatorUganda, loading: app.loading });
+
+type SpotlightProps<T> =
+  Props<T> & {
+      state?: StateToShare,
+      ...BoundState
+  };
+
+const spotlightNavBarTabs = (props: SpotlightProps<SpotLightlIndicator>) => {
+  return (<NavigationBarTabs
+    navBarItems={data.spotlightThemes}
+    showUsingThisViz
+    loading={props.loading}
+    changeActiveIndicator={props.changeActiveIndicator}
+    changeLoadingStatus={props.changeLoadingStatus}
+    activeIndicator={props.state && props.state.indicator ?
+      props.state.indicator : props.activeIndicator}
+  />);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(spotlightNavBarTabs);
