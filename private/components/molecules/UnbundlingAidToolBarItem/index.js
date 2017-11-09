21fff5d4423e3d5c6562a3cf33a534affda0153f
epicallan <epicallan.al@gmail.com>
2017-11-09 15:24:11 +0300

    unbundling aid toobar wip2

--- private/components/molecules/UnbundlingAidToolBarItem/index.js
+++ private/components/molecules/UnbundlingAidToolBarItem/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import Select from 'components/molecules/UnbundlingAidSelect';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 type Props = {
   aid: string,
@@ -13,69 +14,92 @@ type Props = {
   onChange?: (key: string, value: string) => void,
 };
 /* eslint-disable no-unused-vars */
-const ToolBarItem = (props: Props) => {
-  const {
-    width,
-    data,
-    position = 0,
-    values,
-    textAlign,
-    aid,
-    onChange = (key, value) => {
-      // TOFIX: @ernest why is this here yet its unused
-    },
-  } = props;
+export default class ToolBarItem extends React.Component {
+  static reorder(list, startIndex, endIndex) {
+    const [removed] = list.splice(startIndex, 1); // gets a hold of the item
+    list.splice(endIndex, 0, removed); // adds item in new place
+    return list;
+  }
+  static reorder: (list: string[], startIndex: number, endIndex: number) => string[]
+  constructor(props: Props) {
+    super(props);
+    const keys = Object.keys(this.props.data);
+    this.state = {keys};
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+  state: {
+    keys: string[]
+  }
+  onDragEnd: (result: Object) => void
 
-  const keys = Object.keys(data);
+  onDragEnd(result: Object) {
+    // dropped outside the list
+    if (!result.destination) return;
 
-  return (
-    <Grid.Column width={width} textAlign={textAlign || 'right'} verticalAlign="middle">
-      <span>
-        {aid}
-        <Select
-          active
-          value={values[keys.indexOf('years')]}
-          options={data.years}
-          onChange={d => onChange('years', d)}
-        />
-        <Select
-          active={keys.indexOf('to') <= position}
-          value={values[keys.indexOf('to')] || ''}
-          smallText="to"
-          options={data.to}
-          onChange={d => onChange('to', d)}
-        />
-        <Select
-          active={keys.indexOf('from') <= position}
-          value={values[keys.indexOf('from')] || ''}
-          smallText="from"
-          options={data.from}
-          onChange={d => onChange('from', d)}
-        />
-        <Select
-          active={keys.indexOf('sectors') <= position}
-          value={values[keys.indexOf('sectors')] || ''}
-          smallText="sector"
-          options={data.sectors}
-          onChange={d => onChange('sectors', d)}
-        />
-        <Select
-          active={keys.indexOf('forms') <= position}
-          value={values[keys.indexOf('forms')] || ''}
-          smallText="in the form of"
-          options={data.forms}
-          onChange={d => onChange('forms', d)}
-        />
-        <Select
-          active={keys.indexOf('channels') <= position}
-          value={values[keys.indexOf('channels')] || ''}
-          smallText="via channel"
-          options={data.channels}
-          onChange={d => onChange('channels', d)}
-        />
-      </span>
-    </Grid.Column>
-  );
-};
+    const keys = ToolBarItem.reorder(
+      this.state.keys,
+      result.source.index,
+      result.destination.index
+    );
+    this.setState({
+      keys
+    });
+  }
 
-export default ToolBarItem;
+  render() {
+    const {
+      width,
+      data,
+      position = 0,
+      values,
+      textAlign,
+      aid,
+      onChange = (key, value) => {
+        // TOFIX: @ernest why is this here yet its unused
+      },
+    } = this.props;
+    return (
+      <Grid.Column width={width} textAlign={textAlign || 'right'} verticalAlign="middle">
+        <div>
+          <span>{aid}</span>
+          <DragDropContext onDragEnd={this.onDragEnd}>
+            <Droppable droppableId="droppable">
+              {(provided, snapshot) => (
+                <div ref={provided.innerRef}>
+                  {this.state.keys.map(key =>
+                    key === 'years' ?
+                      (<Draggable key={key} draggableId={key}>
+                        <Select
+                          ref={provided.innerRef}
+                          key={key}
+                          active
+                          value={values[this.state.keys.indexOf(key)]}
+                          options={data.years}
+                          onChange={d => onChange(key, d)}
+                        />
+                      </Draggable>) :
+                      (<Draggable key={key} draggableId={key}>
+                        <Select
+                          ref={provided.innerRef}
+                          key={key}
+                          active={this.state.keys.indexOf(key) <= position}
+                          value={values[this.state.keys.indexOf(key)] || ''}
+                          smallText={key}
+                          options={data.to}
+                          onChange={d => onChange(key, d)}
+                        />
+                      </Draggable>)
+                  )
+                  }
+                  {provided.placeholder}
+                </div>
+              )
+              }
+            </Droppable>
+          </DragDropContext>
+        </div>
+      </Grid.Column>
+    );
+  }
+}
+
