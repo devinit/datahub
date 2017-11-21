8af28ed6d8012492da78d9ea515c7afae6f9817a
epicallan <epicallan.al@gmail.com>
2017-11-21 12:08:15 +0300

    map tour storybook fix

--- private/components/molecules/TourContainer/index.stories.js
+++ private/components/molecules/TourContainer/index.stories.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import UnbundlingAidTour from 'components/atoms/UnbundlingAidTour';
-import GlobalVisualizationTour from 'components/molecules/MapTour';
 import GovernmentFinanceTour from 'components/atoms/GovernmentFinanceTour';
+import GlobalVisualizationTour from '../MapTour';
 import Tour from '.';
 
 storiesOf('Tour', module)
