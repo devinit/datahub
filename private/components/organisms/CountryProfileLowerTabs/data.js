28a555b93582f770852634782a1b2aad38c10bc3
epicallan <epicallan.al@gmail.com>
2018-02-01 13:31:04 +0300

    new storyshot

--- private/components/organisms/CountryProfileLowerTabs/data.js
+++ private/components/organisms/CountryProfileLowerTabs/data.js
@@ -1,4 +1,15 @@
 /* eslint-disable */
 // this file is auto generated
 
-module.exports = { internationalResources: null };
+module.exports = {
+  internationalResources: {
+    resourcesOverTime: {
+      toolTip: {
+        heading:
+          'Estimates of international resource flows into the country, 2015 US$. Data is for 2015. Estimates are gross inflows where available, with the following exceptions: foreign direct investment and portfolio equity are net inflows (new investments minus disinvestments) and short-term debt is net flows (disbursements minus capital repayments). See methodology for detailed notes on the data for each resource flow.',
+        source:
+          'Development Initiatives based on numerous sources. Note that data on some flows are missing. Official development assistance and other official flows: OECD DAC. Foreign direct investment: UNCTAD Stats. Long- and short-term debt: World Bank International Debt Statistics. Portfolio equity: World Bank data bank. Remittances: World Bank Migration and Remittances Data.'
+      }
+    }
+  }
+};
