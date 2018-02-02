250cab142b017592c29198f5fe026646108e428a
epicallan <epicallan.al@gmail.com>
2018-02-02 12:16:37 +0300

    finish refactoring base package

---
+++ packages/base/src/__mocks__/graphql.ts
@@ -0,0 +1,2 @@
+const graphqlQueryStub: string = '';
+module.exports = graphqlQueryStub;
---
+++ packages/base/src/workers/index.ts
@@ -0,0 +1,22 @@
+/**
+ * runs in the front paget template and pulls in global picture data for client caching
+ */
+import {getData} from '../utils';
+
+interface ICacheOpts {
+  uri: string;
+  query: string;
+  api: string;
+  data: Array<{indicators: Array<{id: string}>}>;
+}
+export const cacheData = ({uri, query, api, data}: ICacheOpts) => {
+  data.forEach(item => {
+    if (!item.indicators) throw Error('indicators missing in navItem');
+    item.indicators.forEach((indicator) => {
+      const variables = { id: indicator.id };
+      getData({query, variables, uri})
+        .then(() => console.log(`cahing map data for ${indicator.id || 'nothing'}`))
+        .catch((error) => console.error(`failed to cach data for ${indicator.id || 'nothing'}`, error));
+    });
+  });
+};
