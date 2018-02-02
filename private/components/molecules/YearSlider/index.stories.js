b49743b0fa3391677f2861a1b6a141b8aaff122d
epicallan <epicallan.al@gmail.com>
2018-02-02 12:54:27 +0300

    minor change

--- private/components/molecules/YearSlider/index.stories.js
+++ private/components/molecules/YearSlider/index.stories.js
@@ -3,6 +3,9 @@ import { storiesOf } from '@storybook/react';
 import Slider from '.';
 
 const onChange = year => console.info(`current slider year ${year}`);
-storiesOf('Year Slider', module).add('Datahub Year Slider', () =>
-  <Slider minimum={2000} maximum={2020} step={1} position={2016} onChange={onChange} />,
-);
+storiesOf('Year Slider', module)
+  .add('Datahub Year Slider', () =>
+    <div style={{width: '40%', marginTop: '50px', marginLeft: '100px'}}>
+      <Slider minimum={2000} maximum={2020} step={1} position={2016} onChange={onChange} />
+    </div>
+  );
