dd95b1465b7df711ddca1a9067dda90e65633e26
epicallan <epicallan.al@gmail.com>
2018-02-06 10:52:26 +0300

    base package changes to make ts happy

---
+++ packages/base/src/types.ts
@@ -0,0 +1,60 @@
+
+export interface IProcess {
+  version: string;
+  browser: boolean;
+    // @ts-ignore
+  config: Process['config'] & {
+    api: string;
+    old_datahub: string;
+  };
+  env: any & {
+    PORT: number,
+    NODE_ENV: string;
+  };
+}
+
+// @ts-ignore
+export declare var process: IProcess;
+
+export interface District {
+  id: string;
+  name: string;
+  slug?: string;
+}
+
+export type Country = District & {
+  has_domestic_data?: string,
+  countryType: string,
+  hasPDF?: boolean
+};
+
+export interface NavIndicator {
+    id: string;
+    name: string;
+    heading?: string;
+    tooltip?: string;
+    source?: string;
+}
+
+export interface NavBarItem {
+  id: string;
+  name: string;
+  indicators: NavIndicator[];
+  default_indicator?: string;
+}
+
+export interface SelectOption {
+  id: string;
+  name: string;
+  text: string;
+}
+
+export interface MenueItem {
+    name: string;
+    link: string;
+    icon?: string;
+    children?: MenueItem[];
+}
+export interface Menue {
+    mainMenu: MenueItem[];
+}
