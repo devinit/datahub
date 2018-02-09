95e8ad5e7dc534227a62b2a252bd0a604c21faf7
epicallan <epicallan.al@gmail.com>
2018-02-09 15:58:18 +0300

    wip spotlights correction

---
+++ packages/ui/src/molecules/SpotLightTabs/types.ts
@@ -0,0 +1,9 @@
+import '@devinit/datahub-api';
+
+export interface SpotLightTabData {
+    povertyTabRegional: DH.PovertyTabRegional;
+    overviewTabRegional: DH.IOverviewTabRegional;
+    populationTabRegional: DH.PopulationTabRegional;
+    educationTabRegional: DH.EducationTabRegional;
+    healthTabRegional: DH.HealthTabRegional;
+}
