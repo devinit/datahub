aeb715e7e1bf0120c77a31759a98aac394737342
epicallan <epicallan.al@gmail.com>
2018-02-19 21:02:37 +0300

    correcting / working though ui storybooks

---
+++ packages/ui/src/molecules/SocialMediaBar/index.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import SocialMediaBar from '.';
+
+storiesOf('SocialMedia Bar DontTest', module)
+    .add('bar', () => <SocialMediaBar />);
