3fcee61948ada6b79561f2955eb0dc52ab5aefb6
epicallan <epicallan.al@gmail.com>
2018-02-19 16:18:56 +0300

    getting storybook to work

---
+++ packages/ui/src/molecules/Footer/index.story.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import Footer from '.';
+
+storiesOf('Footer', module).add('Datahub Footer', () => <Footer />);
---
+++ packages/ui/src/molecules/LoadingBar/index.story.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import Loader from '.';
+
+storiesOf('Loader', module).add('progress loader', () => {
+  const props = { loading: true };
+  return <Loader {...props} />;
+});
