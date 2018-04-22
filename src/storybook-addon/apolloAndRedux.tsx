6b135315c5b56a88c7739809394ccd37a90d2fde
Lukwago Allan <epicallan.al@gmail.com>
2018-04-22 09:35:36 +0300

    Update apolloAndRedux.tsx

--- src/storybook-addon/apolloAndRedux.tsx
+++ src/storybook-addon/apolloAndRedux.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { initRedux } from '../redux';
 import { client } from './apollo';
 
-// APollo provide comes with redux provider
 const withApolloAndReduxProvider = () => {
   return storyFn => {
     return (
