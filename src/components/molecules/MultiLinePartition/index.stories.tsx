2dfdb031ccd1c9c2f35a804d52f681bb1ddbbbc7
epicallan <epicallan.al@gmail.com>
2018-04-25 15:03:25 +0300

    add country government finance story

--- src/components/molecules/MultiLinePartition/index.stories.tsx
+++ src/components/molecules/MultiLinePartition/index.stories.tsx
@@ -4,17 +4,22 @@ import MultiLinePartition, {Props} from '.';
 import config from '../../visbox/localLinePartition';
 const ugData = require('./testData/ug.json');
 const keData = require ('./testData/ke.json');
+const ngData = require ('./testData/ng.json');
 
-const getProps = (data): Props => {
-    const governmentFinance = data.localGovernmentFinance;
+const getProps = (data: any, financeType: string): Props => {
+    const governmentFinance = data[financeType];
     const items: Array<{title: string, data: DH.IDomestic[], inverted?: boolean}> = [];
     const revenueAndGrants = governmentFinance.revenueAndGrants as DH.IDomestic[];
     const expenditure = governmentFinance.expenditure as DH.IDomestic[];
+    if (financeType === 'governmentFinance') {
+        const finance = governmentFinance.finance as DH.IDomestic[];
+        items.push({title: 'Finance', data: finance, inverted: false});
+    }
     if (revenueAndGrants.length) items.push({title: 'Revenue', data: revenueAndGrants});
     if (expenditure.length) items.push({title: 'Expenditure', data: expenditure, inverted: false});
     return {
         loading: false,
-        chartId: `${governmentFinance.currencyCode}-localGovmntChart`,
+        chartId: `${governmentFinance.currencyCode}-GovmntChart`,
         config,
         currencyCode: governmentFinance.currencyCode || '',
         currencyUSD: governmentFinance.currencyUSD || '' ,
@@ -24,5 +29,6 @@ const getProps = (data): Props => {
 };
 
 storiesOf('MultiLine partition', module)
-  .add('Kampala', () => <MultiLinePartition {...getProps(ugData.data)}/>)
-  .add('Nairobi', () => <MultiLinePartition {...getProps(keData.data)}/>);
+  .add('Kampala', () => <MultiLinePartition {...getProps(ugData.data, 'localGovernmentFinance')}/>)
+  .add('Nairobi', () => <MultiLinePartition {...getProps(keData.data, 'localGovernmentFinance')}/>)
+  .add('Nigeria', () => <MultiLinePartition {...getProps(ngData.data, 'governmentFinance')}/>);
