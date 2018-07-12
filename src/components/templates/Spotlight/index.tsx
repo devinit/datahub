6949c16fd072d24db73a8809d2b7b2411e9126af
Edwin P. Magezi <edwinm_p@yahoo.com>
2018-07-12 14:41:08 +0300

    Add link to country page from spotlight page

--- src/components/templates/Spotlight/index.tsx
+++ src/components/templates/Spotlight/index.tsx
@@ -1,60 +1,85 @@
-import * as React from 'react';
-import { Div } from 'glamorous';
-import { Container, Grid, Icon} from 'semantic-ui-react';
-import SpotLightNavTabsKe, {SpotlightKeProps} from '../../organisms/NavBarTabs/spotlightKe';
-import SpotLightNavTabsUg, {SpotlightUgProps} from '../../organisms/NavBarTabs/spotlightUg';
-import About from '../../molecules/About';
-import {capitalize} from '@devinit/prelude/lib/strings';
-import {cacheMapData} from '../../../utils';
-import {StateToShare} from '../../molecules/ChartShare';
-import DynamicMap from '../../organisms/Map/DynamicMap';
-import Generic from '../Generic';
+import { capitalize } from '@devinit/prelude/lib/strings';
+import { Div, H4, Img } from 'glamorous';
 import router from 'next/router';
-import {getSpotlightPageData} from '../../pageData';
-
-interface Props  {
+import * as React from 'react';
+import { Container, Grid, Icon } from 'semantic-ui-react';
+import { cacheMapData, getCountry } from '../../../utils';
+import About from '../../molecules/About';
+import { StateToShare } from '../../molecules/ChartShare';
+import DynamicMap from '../../organisms/Map/DynamicMap';
+import SpotLightNavTabsKe, { SpotlightKeProps } from '../../organisms/NavBarTabs/spotlightKe';
+import SpotLightNavTabsUg, { SpotlightUgProps } from '../../organisms/NavBarTabs/spotlightUg';
+import { getSpotlightPageData } from '../../pageData';
+import { Country } from '../../types';
+import Generic from '../Generic';
+import Link from 'next/link';
+import { red } from '../../theme/semantic';
+interface Props {
   pathname: string;
   id: string;
   state: StateToShare;
 }
 
 export default class Spotlight extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
-  public componentDidMount() {
+  country?: Country = getCountry(this.props.id);
+
+  componentDidMount() {
     cacheMapData(`/worker_${this.props.id}.js`);
   }
-  public render() {
+
+  render() {
     const countryName = capitalize(this.props.id);
     // this casting is hack, typecript couldnt infar correct types
-    const navState = {state: this.props.state} as SpotlightKeProps | SpotlightUgProps;
+    const navState = { state: this.props.state } as SpotlightKeProps | SpotlightUgProps;
+
     return (
       <Generic >
         <Container>
-          <Div paddingTop={'4em'} paddingBottom={'4em'}>
+          <Div paddingTop={ '2em' } paddingBottom={ '4em' }>
+            <H4 color="red">
+              { this.renderCountryFlag() }
+              <Link href={ `/country/${this.props.id}` }>
+                <a role="link" style={ { color: red } }>{ countryName }</a>
+              </Link>
+            </H4>
             <Grid centered>
-              <Grid.Column width={12} textAlign="center">
+              <Grid.Column width={ 12 } textAlign="center">
                 <b>
-                  <Icon name="pie graph" /> Spotlight on {countryName} {' '}
+                  <Icon name="pie graph" /> Spotlight on { countryName } { ' ' }
                 </b>
-                {getSpotlightPageData(this.props.id)}
+                { getSpotlightPageData(this.props.id) }
               </Grid.Column>
             </Grid>
           </Div>
         </Container>
-        {this.props.id === 'uganda' ?
-          <SpotLightNavTabsUg {...navState} />
+        { this.props.id === 'uganda' ?
+          <SpotLightNavTabsUg { ...navState } />
           :
-          <SpotLightNavTabsKe {...navState} />
+          <SpotLightNavTabsKe { ...navState } />
         }
 
         {
           process.env.NODE_ENV !== 'test' && (process as any).browser ?
-            <DynamicMap country={this.props.id} state={this.props.state} /> : ''
+            <DynamicMap country={ this.props.id } state={ this.props.state } /> : ''
         }
-        <About router={router} />
+        <About router={ router } />
       </Generic>
     );
+  }
+
+  private renderCountryFlag(): React.ReactElement<any> | null {
+    if (this.country) {
+      return (
+        <Img
+          width="16px"
+          verticalAlign="middle"
+          src={ `/flags/svg/${this.country.id}.svg` }
+          marginRight="5px"
+          marginBottom="3px"
+        />
+      );
+    }
+
+    return null;
   }
 }
