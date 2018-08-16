f885e9ff1fec7cf54f00f061e0457440b4d41aa8
Edwin P. Magezi <edwinm_p@yahoo.com>
2018-08-16 14:13:38 +0300

    Update next.js 6.1.1

--- src/components/organisms/NavBarTabs/spotlightUg.tsx
+++ src/components/organisms/NavBarTabs/spotlightUg.tsx
@@ -1,39 +1,37 @@
+import { State } from '../../../redux/reducers';
 import * as React from 'react';
-import { changeSpotlightIndicatorUg, changeLoadingStatus, SpotLightlIndicatorUg,
-  LoadingStatus} from '../../../redux/actions';
-import { bindActionCreators, Dispatch, AnyAction } from 'redux';
+import { AnyAction, Dispatch, bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { } from '../../actions';
-import { State} from '../../../redux/reducers';
-import { Props } from '../../molecules/NavigationBarTabs';
-import NavigationBarTabs from '../../molecules/NavigationBarTabs';
-import {StateToShare} from '../../molecules/ChartShare';
-import {BoundAction, BoundState} from './types';
+import { LoadingStatus, SpotLightlIndicatorUg, changeLoadingStatus,
+  changeSpotlightIndicatorUg } from '../../../redux/actions';
+import NavigationBarTabs, { Props } from '../../molecules/NavigationBarTabs';
+import { StateToShare } from '../../molecules/ChartShare';
+import { BoundAction, BoundState } from './types';
 import data from './uganda';
 
 const mapDispatchToProps = (dispatch: Dispatch<AnyAction>): BoundAction<SpotLightlIndicatorUg> =>
 ({
   changeActiveIndicator: bindActionCreators(changeSpotlightIndicatorUg, dispatch),
-  changeLoadingStatus: bindActionCreators(changeLoadingStatus, dispatch),
+  changeLoadingStatus: bindActionCreators(changeLoadingStatus, dispatch)
 });
 
 const mapStateToProps = ({ app }: State): BoundState =>
   ({ activeIndicator: app.indicatorUganda, loading: app.loading });
 
-export type SpotlightUgProps = Props<SpotLightlIndicatorUg,  LoadingStatus> & {state?: StateToShare}
+export type SpotlightUgProps = Props<SpotLightlIndicatorUg, LoadingStatus> & {state?: StateToShare}
   & BoundAction<SpotLightlIndicatorUg>;
 
 // TODO: fix types
 const spotlightNavBarTabs: React.SFC<any> = (props) => {
   return (<NavigationBarTabs
-    navBarItems={data.spotlightThemes}
+    navBarItems={ data.spotlightThemes }
     showUsingThisViz
     isForSpotlightsUg
-    loading={props.loading}
-    changeActiveIndicator={props.changeActiveIndicator}
-    changeLoadingStatus={props.changeLoadingStatus}
-    activeIndicator={props.state && props.state.indicator ?
-      props.state.indicator : props.activeIndicator}
+    loading={ props.loading }
+    changeActiveIndicator={ props.changeActiveIndicator }
+    changeLoadingStatus={ props.changeLoadingStatus }
+    activeIndicator={ props.state && props.state.indicator ?
+      props.state.indicator : props.activeIndicator }
   />);
 };
 
