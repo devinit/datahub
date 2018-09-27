ac8ea275272bffdb23604dbb4989a18e6591f01b
Edwin P. Magezi <edwinm_p@yahoo.com>
2018-09-27 14:57:09 +0300

    [New] add navigation links to spotlight tabs (issue #418)

--- src/components/organisms/NavBarTabs/spotlightKe.tsx
+++ src/components/organisms/NavBarTabs/spotlightKe.tsx
@@ -1,39 +1,49 @@
 import * as React from 'react';
-import { changeSpotlightIndicatorKe, changeLoadingStatus,
-  SpotLightlIndicatorKe,  LoadingStatus} from '../../../redux/actions';
-import { bindActionCreators, Dispatch, AnyAction} from 'redux';
 import { connect } from 'react-redux';
-import { Props } from '../../molecules/NavigationBarTabs';
-import { State} from '../../../redux/reducers';
-import NavigationBarTabs from '../../molecules/NavigationBarTabs';
-import {StateToShare} from '../../molecules/ChartShare';
-import {BoundAction, BoundState} from './types';
+import { AnyAction, Dispatch, bindActionCreators } from 'redux';
+import {
+  LoadingStatus,
+  SpotLightlIndicatorKe,
+  changeLoadingStatus,
+  changeSpotlightIndicatorKe } from '../../../redux/actions';
+import { State } from '../../../redux/reducers';
+import { StateToShare } from '../../molecules/ChartShare';
+import NavigationBarTabs, { Props } from '../../molecules/NavigationBarTabs';
 import data from './kenya';
+import { BoundAction, BoundState } from './types';
 
 const mapDispatchToProps = (dispatch: Dispatch<AnyAction>): BoundAction<SpotLightlIndicatorKe> =>
   ({
     changeActiveIndicator: bindActionCreators(changeSpotlightIndicatorKe, dispatch),
-    changeLoadingStatus: bindActionCreators(changeLoadingStatus, dispatch),
+    changeLoadingStatus: bindActionCreators(changeLoadingStatus, dispatch)
   });
 
-export type SpotlightKeProps = Props<SpotLightlIndicatorKe,  LoadingStatus> & {state?: StateToShare}
+export type SpotlightKeProps = Props<SpotLightlIndicatorKe, LoadingStatus> & {state?: StateToShare}
   & BoundAction<SpotLightlIndicatorKe>;
 
-const mapStateToProps = ({ app }: State): BoundState =>
-  ({ activeIndicator: app.indicatorKenya, loading: app.loading });
+const mapStateToProps = ({ app }: State): BoundState => ({ activeIndicator: app.indicatorKenya, loading: app.loading });
 
-// TODO: fix types
-const spotlightNavBarTabs: React.SFC<any> = (props) => {
-  return (<NavigationBarTabs
-    navBarItems={data.spotlightThemes}
-    showUsingThisViz
-    loading={props.loading}
-    isForSpotlightsKe
-    changeActiveIndicator={props.changeActiveIndicator}
-    changeLoadingStatus={props.changeLoadingStatus}
-    activeIndicator={props.state && props.state.indicator ?
-      props.state.indicator : props.activeIndicator}
-  />);
-};
+class SpotlightNavBarTabs extends React.Component<SpotlightKeProps> {
+  componentDidMount() {
+    const { state } = this.props;
+    if (state && state.indicator && this.props.activeIndicator !== state.indicator) {
+      this.props.changeActiveIndicator(state.indicator);
+    }
+  }
 
-export default connect(mapStateToProps, mapDispatchToProps)(spotlightNavBarTabs);
+  render() {
+    return (
+      <NavigationBarTabs
+        navBarItems={ data.spotlightThemes }
+        showUsingThisViz
+        loading={ this.props.loading }
+        isForSpotlightsKe
+        changeActiveIndicator={ this.props.changeActiveIndicator }
+        changeLoadingStatus={ this.props.changeLoadingStatus }
+        activeIndicator={ this.props.activeIndicator }
+      />
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SpotlightNavBarTabs) as any;
--- src/components/organisms/NavBarTabs/spotlightUg.tsx
+++ src/components/organisms/NavBarTabs/spotlightUg.tsx
@@ -1,40 +1,51 @@
 import * as React from 'react';
-import { changeSpotlightIndicatorUg, changeLoadingStatus, SpotLightlIndicatorUg,
-  LoadingStatus} from '../../../redux/actions';
-import { bindActionCreators, Dispatch, AnyAction } from 'redux';
 import { connect } from 'react-redux';
-import { } from '../../actions';
-import { State} from '../../../redux/reducers';
-import { Props } from '../../molecules/NavigationBarTabs';
-import NavigationBarTabs from '../../molecules/NavigationBarTabs';
-import {StateToShare} from '../../molecules/ChartShare';
-import {BoundAction, BoundState} from './types';
+import { AnyAction, Dispatch, bindActionCreators } from 'redux';
+import {
+  LoadingStatus,
+  SpotLightlIndicatorUg,
+  changeLoadingStatus,
+  changeSpotlightIndicatorUg } from '../../../redux/actions';
+import { State } from '../../../redux/reducers';
+import { StateToShare } from '../../molecules/ChartShare';
+import NavigationBarTabs, { Props } from '../../molecules/NavigationBarTabs';
+import { BoundAction, BoundState } from './types';
 import data from './uganda';
 
 const mapDispatchToProps = (dispatch: Dispatch<AnyAction>): BoundAction<SpotLightlIndicatorUg> =>
 ({
   changeActiveIndicator: bindActionCreators(changeSpotlightIndicatorUg, dispatch),
-  changeLoadingStatus: bindActionCreators(changeLoadingStatus, dispatch),
+  changeLoadingStatus: bindActionCreators(changeLoadingStatus, dispatch)
 });
 
 const mapStateToProps = ({ app }: State): BoundState =>
   ({ activeIndicator: app.indicatorUganda, loading: app.loading });
 
-export type SpotlightUgProps = Props<SpotLightlIndicatorUg,  LoadingStatus> & {state?: StateToShare}
+export type SpotlightUgProps = Props<SpotLightlIndicatorUg, LoadingStatus> & { state?: StateToShare }
   & BoundAction<SpotLightlIndicatorUg>;
 
 // TODO: fix types
-const spotlightNavBarTabs: React.SFC<any> = (props) => {
-  return (<NavigationBarTabs
-    navBarItems={data.spotlightThemes}
-    showUsingThisViz
-    isForSpotlightsUg
-    loading={props.loading}
-    changeActiveIndicator={props.changeActiveIndicator}
-    changeLoadingStatus={props.changeLoadingStatus}
-    activeIndicator={props.state && props.state.indicator ?
-      props.state.indicator : props.activeIndicator}
-  />);
-};
+class SpotlightNavBarTabs extends React.Component<SpotlightUgProps> {
+  componentDidMount() {
+    const { state } = this.props;
+    if (state && state.indicator && this.props.activeIndicator !== state.indicator) {
+      this.props.changeActiveIndicator(state.indicator);
+    }
+  }
 
-export default connect(mapStateToProps, mapDispatchToProps)(spotlightNavBarTabs);
+  render() {
+    return (
+      <NavigationBarTabs
+        navBarItems={ data.spotlightThemes }
+        showUsingThisViz
+        isForSpotlightsUg
+        loading={ this.props.loading }
+        changeActiveIndicator={ this.props.changeActiveIndicator }
+        changeLoadingStatus={ this.props.changeLoadingStatus }
+        activeIndicator={ this.props.activeIndicator }
+      />
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SpotlightNavBarTabs) as any;
